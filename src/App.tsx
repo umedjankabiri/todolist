41a51bc5e62910a0e1e0ed489611afb949f3fff9
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 02:51:35 +0300

    added Todolist component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
+import { Todolist } from 'common/components/Todolist/Todolist';
 import './App.css'
 
 function App() {
   return (
       <div className="App">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-            <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-            <li><input type="checkbox" checked={false}/> <span>React</span></li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
+        <Todolist/>
+        <Todolist/>
+        <Todolist/>
       </div>
   );
 }
