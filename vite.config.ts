ad3d1273650b1770b850ab0305f8dd892da59e88
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 00:14:31 +0300

    added aliases for absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/common/components'),
+      types: path.resolve(__dirname, 'src/common/types'),
+    }
+  }
 })
