8a580561c912721a1330fb9c7114786c985ea75f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 20:32:08 +0300

    property "onClick" has been called in Button component

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import {ButtonProps} from "common/types/Button/ButtonProps.ts";
 import {FC} from "react";
 
 export const Button: FC<ButtonProps> = (props) => {
-    const onClickHandler = ()=> props.onClick
+    const onClickHandler = ()=> props.onClick && props.onClick()
 
     return (
         <button className={props.className} onClick={onClickHandler}>{props.title}</button>
