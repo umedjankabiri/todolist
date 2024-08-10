fa2061830d2e30e641a68dd8d77f542e6e93fd64
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 03:37:03 +0300

    deleted one of the arrays for demonstration conditional render

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -1,17 +1,14 @@
 import 'common/components/App/App.css'
 import { Todolist } from 'common/components/Todolist/Todolist.tsx';
+import {TaskProps} from "common/types/Tasks/TaskProps.ts";
 
 function App() {
-    const tasks1 = [
+    const tasks1: TaskProps[] = [
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'ReactJS', isDone: false},
     ]
-    const tasks2 = [
-        {id: 1, title: 'Hello world', isDone: true},
-        {id: 2, title: 'I am Happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false},
-    ]
+    const tasks2: TaskProps[] = []
 
   return (
       <div className="App">
