8dee631ef3a9ad4156f9f6fc67785098b54e4ef4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 03:17:22 +0300

    added arrays tasks1 and tasks2, used arrays of tasks in Todolist component

--- src/common/components/App/App.tsx
+++ src/common/components/App/App.tsx
@@ -1,12 +1,28 @@
-import { Todolist } from 'common/components/Todolist/Todolist.tsx';
 import 'common/components/App/App.css'
+import { Todolist } from 'common/components/Todolist/Todolist.tsx';
 
 function App() {
+    const tasks1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+    ]
+    const tasks2 = [
+        {id: 1, title: 'Hello world', isDone: true},
+        {id: 2, title: 'I am Happy', isDone: false},
+        {id: 3, title: 'Yo', isDone: false},
+    ]
+
   return (
       <div className="App">
-        <Todolist title="what to leand"/>
-        <Todolist title = "Songs" />
-        <Todolist title = "Books" />
+        <Todolist
+            title="what to leand"
+            tasks={tasks1}
+        />
+        <Todolist
+            title = "Songs"
+            tasks={tasks2}
+        />
       </div>
   );
 }
