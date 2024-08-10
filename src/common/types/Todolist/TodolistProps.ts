4cfc26c5dd2341b2f4403a4b20f459911c2264d0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 13:13:32 +0300

    created callback "removeTask", "changedFilter" properties for TodolistProps type

--- src/common/types/Todolist/TodolistProps.ts
+++ src/common/types/Todolist/TodolistProps.ts
@@ -1,6 +1,9 @@
 import {TaskProps} from "common/types/Tasks/TaskProps.ts";
+import {FilterValuesProps} from "common/types/Todolist/FilterValuesProps.ts";
 
 export type TodolistProps = {
     title: string
     tasks: TaskProps[]
+    removeTask: (taskID: number) => void
+    changedFilter: (filtered: FilterValuesProps) => void
 }
