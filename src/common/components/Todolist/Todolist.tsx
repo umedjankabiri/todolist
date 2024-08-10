4e8f9d43550472455510196e2a3e9e188ae8cc28
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-10 13:36:11 +0300

    created Button "x" for remove tasks added callback function "onClickRemoveTask" for "x" Button

--- src/common/components/Todolist/Todolist.tsx
+++ src/common/components/Todolist/Todolist.tsx
@@ -4,13 +4,17 @@ import {FC} from "react";
 
 export const Todolist: FC<TodolistProps> = (props) => {
     const mappedTasks = props.tasks.map(task => {
+        const onClickRemoveTask = () => props.removeTask(task.id)
+
         return (
             <li key={task.id}>
                 <input type="checkbox" checked={task.isDone} />
                 <span>{task.title}</span>
+                <Button title={"x"} onClick={onClickRemoveTask} />
             </li>
         )
     })
+
 
     return (
         <div>
@@ -23,7 +27,7 @@ export const Todolist: FC<TodolistProps> = (props) => {
                 { props.tasks.length > 0 ? mappedTasks : <span>No tasks</span> }
             </ul>
             <div>
-                <Button title={"All"} />
+                <Button title={"All"}/>
                 <Button title={"Active"}/>
                 <Button title={"Completed"}/>
             </div>
