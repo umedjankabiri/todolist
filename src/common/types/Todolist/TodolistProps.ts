19f48b42ba5c7ee372ab5721a2f7d7fc2f5d2cd3
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-17 00:40:03 +0300

    added "todolistID" property to TodolistProps type, and enhance callback function "changeFilter" arguments

--- src/common/types/Todolist/TodolistProps.ts
+++ src/common/types/Todolist/TodolistProps.ts
@@ -2,10 +2,11 @@ import {TaskProps} from "common/types/Tasks/TaskProps.ts";
 import {FilterValueProps} from "common/types/Tasks/FilterValueProps.ts";
 
 export type TodolistProps = {
+    todolistID: string
     title: string
     tasks: TaskProps[]
     removeTask: (taskID: string) => void
-    changeFilter: (filtered: FilterValueProps) => void
+    changeFilter: (todolistID: string, filtered: FilterValueProps) => void
     addTask: (title: string) => void
     changeTaskStatus: (taskID: string, taskStatus: boolean) => void
     filter: string
