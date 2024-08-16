ede3ef683d73d4bd24528ffa362b9203120c7fd5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-17 00:03:33 +0300

    created a TodolistsProps type file for typing array of Todolists

---
+++ src/common/components/Todolist/TodolistsProps.ts
@@ -0,0 +1,7 @@
+import {FilterValueProps} from "common/types/Tasks/FilterValueProps.ts";
+
+export type TodolistsProps = {
+    todolistID: string
+    title: string
+    filter: FilterValueProps
+}
