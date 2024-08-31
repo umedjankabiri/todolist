f857fe4cc11b5377c5c569d6fc4958d4eba29496
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-08-31 05:58:16 +0300

    Enable text editing in EditableSpan component
    Refactor EditableSpan to allow real-time text changes. Added state for title and handlers for input change and blur events to update the title dynamically.

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -1,19 +1,27 @@
-import {FC, useState} from "react";
+import {ChangeEvent, FC, useState} from "react";
 import {EditableSpanProps} from "common/types/EditableSpan/EditableSpanProps.ts";
 
 export const EditableSpan: FC<EditableSpanProps> = (props) => {
     const [editMode, setEditMode] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>(props.title)
 
     const activateEditModeHandler = () => {
         setEditMode(true);
     }
     const deactivateEditModeHandler = () => {
         setEditMode(false);
+        props.onChangeTitle(title);
+    }
+    const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>)=> {
+        setTitle(event.currentTarget.value)
     }
 
     return (
         editMode
-            ? <input value={props.title} onBlur={deactivateEditModeHandler} autoFocus/>
+            ? <input value={title}
+                     onBlur={deactivateEditModeHandler}
+                     onChange={changeTitleHandler}
+                     autoFocus/>
             : <span onDoubleClick={activateEditModeHandler}>{props.title}</span>
     );
 };
