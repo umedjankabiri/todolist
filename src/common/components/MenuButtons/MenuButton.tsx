9df86144113eedb0df7cbad1d3c848bd01d2a672
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-07 04:15:09 +0300

    Update MenuButton styling and active state behavior
    Modified the box-shadow and background color for the MenuButton component. Added an active state to apply a transform and change the box-shadow on click.

--- src/common/components/MenuButtons/MenuButton.tsx
+++ src/common/components/MenuButtons/MenuButton.tsx
@@ -5,11 +5,15 @@ import {MenuButtonProps} from "common/types/MenuBottons/MenuButtonProps.ts";
 export const MenuButton = styled(Button)<MenuButtonProps>((props) => ({
     minWidth: "110px",
     fontWeight: "bold",
-    boxShadow: `0 0 0 2px ${props.theme.palette.primary.dark}, 4px 4px 0 0 ${props.theme.palette.primary.light}`,
+    boxShadow: `4px 4px 10px 0.5px ${"#504e4e"}`,
     borderRadius: "2px",
     textTransform: "capitalize",
     color: props.theme.palette.primary.contrastText,
-    backgroundColor: props.background || "#1565C0",
+    backgroundColor: props.background || "#087EA4",
     padding: "8px 24px",
     margin: "0 10px",
+    "&:active": {
+        transform: "translateY(2px)",
+        boxShadow: `0 0 6px ${"#504e4e"}`,
+    }
 }))
