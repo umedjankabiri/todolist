8314788f911b72edfa496e455773ee742e11ebea
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-08 04:51:14 +0300

    Add test for changing todolist filter
    Introduced a new test case to verify the filter change functionality in todolist reducer. Ensures the correct filter is applied to the specified todolist ID.

--- src/model/todolistsReducer/todolistsReducer.test.ts
+++ src/model/todolistsReducer/todolistsReducer.test.ts
@@ -60,3 +60,23 @@ test("Correct todolist should be changed it's title", ()=> {
     expect(endState[0].title).toBe("What to read");
     expect(endState[1].title).toBe(action.payload.title);
 })
+test("Correct filter of todolist should be changed", ()=> {
+    let todolistID1 = v1()
+    let todolistID2 = v1()
+    const initialState: TodolistsProps[] = [
+        {todolistID: todolistID1, title: "What to read", filter: "All"},
+        {todolistID: todolistID2, title: "What to buy", filter: "All"},
+    ]
+
+    const action = {
+        type: "CHANGE-TODOLIST-FILTER",
+        payload: {
+            todolistID: todolistID2,
+            filter: "Completed",
+        }
+    } as const
+    const endState: TodolistsProps[] = todolistsReducer(initialState, action);
+
+    expect(endState[0].filter).toBe("All");
+    expect(endState[1].filter).toBe(action.payload.filter);
+})
