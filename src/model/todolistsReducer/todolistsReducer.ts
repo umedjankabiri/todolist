35b36e00355dc070aecee859c7acaf822562deff
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-08 04:51:52 +0300

    Refactor todolistReducer and update action types
    Renamed ActionsProps to TodolistsActionsProps for clarity. Refactored the ADD-TODOLIST case to correctly structure the new todo list item. Added cases for changing the todolist title and filter.

--- src/model/todolistsReducer/todolistsReducer.ts
+++ src/model/todolistsReducer/todolistsReducer.ts
@@ -1,6 +1,6 @@
 import {v1} from "uuid";
 import {TodolistsProps} from "common/types/Todolists/TodolistsProps.ts";
-import {ActionsProps} from "model/reducersTypes/TodolistsReducer/ActionsProps.ts";
+import {TodolistsActionsProps} from "model/reducersTypes/TodolistsReducer/TodolistsActionsProps.ts";
 
 // Business Logic Layer (BLL)
 let todolistID1 = v1()
@@ -10,16 +10,29 @@ const initialState: TodolistsProps[] = [
     {todolistID: todolistID2, title: "What to buy", filter: "All"},
 ]
 
-export const todolistsReducer = (state: TodolistsProps[] = initialState, action: ActionsProps) => {
+export const todolistsReducer = (state: TodolistsProps[] = initialState, action: TodolistsActionsProps) => {
     switch (action.type) {
         case "REMOVE-TODOLIST":
             return state.filter(todolist => todolist.todolistID !== action.payload.todolistID)
-        case "ADD-TODOLIST":
-            return {
-                ...state,
-                title: action.payload.title
+        case "ADD-TODOLIST": {
+            const newTodolist: TodolistsProps = {
+                todolistID: v1(),
+                title: action.payload.title,
+                filter: "All"
             }
+            return [newTodolist, ...state];
+        }
+        case "CHANGE-TODOLIST-TITLE":
+            return state.map(todolist => todolist.todolistID === action.payload.todolistID
+                ? {...todolist, title: action.payload.title}
+                : todolist
+            )
+        case "CHANGE-TODOLIST-FILTER":
+            return state.map(todolist => todolist.todolistID === action.payload.todolistID
+                ? {...todolist, filter: action.payload.filter}
+                : todolist
+            )
         default:
-            throw new Error("Unknown action type " + action.type)
+            throw new Error("Unknown action type")
     }
 }
