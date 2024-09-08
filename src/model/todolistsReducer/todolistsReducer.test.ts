f9b2988da2cf22eaccd68f521bcc547edb92ec54
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-08 04:50:49 +0300

    Add test for changing the title of a specific todolist
    Introduced a new test case to verify that the correct todolist's title is updated when a "CHANGE-TODOLIST-TITLE" action is dispatched. This ensures consistency and correctness of title updates within the todolists reducer.

--- src/model/todolistsReducer/todolistsReducer.test.ts
+++ src/model/todolistsReducer/todolistsReducer.test.ts
@@ -40,3 +40,23 @@ test("Correct todolist should be added", ()=> {
     expect(endState.length).toBe(3);
     expect(endState[0].title).toBe(action.payload.title);
 })
+test("Correct todolist should be changed it's title", ()=> {
+    let todolistID1 = v1()
+    let todolistID2 = v1()
+    const initialState: TodolistsProps[] = [
+        {todolistID: todolistID1, title: "What to read", filter: "All"},
+        {todolistID: todolistID2, title: "What to buy", filter: "All"},
+    ]
+
+    const action = {
+        type: "CHANGE-TODOLIST-TITLE",
+        payload: {
+            todolistID: todolistID2,
+            title: "What to learn",
+        }
+    } as const
+    const endState: TodolistsProps[] = todolistsReducer(initialState, action);
+
+    expect(endState[0].title).toBe("What to read");
+    expect(endState[1].title).toBe(action.payload.title);
+})
