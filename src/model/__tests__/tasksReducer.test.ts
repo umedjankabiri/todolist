0eaa12cb23f345b947647e542f85434ef758f8a1
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 17:32:59 +0300

    Add unit test for task removal in tasksReducer
    This commit introduces a unit test to verify correct task deletion within the tasksReducer. The test ensures tasks are removed accurately from the specified array, maintaining state integrity across different to-do lists.

---
+++ src/model/__tests__/tasksReducer.test.ts
@@ -0,0 +1,30 @@
+import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
+import {removeTaskAC, tasksReducer} from "model/tasksReducer/tasksReducer.ts";
+
+test("correct task should be deleted from correct array", ()=> {
+    const startState: TasksStateProps = {
+        todolistID1: [
+            {id: '1', title: "CSS", isDone: false},
+            {id: '2', title: "JS", isDone: true},
+            {id: '3', title: "React", isDone: false}
+        ],
+        todolistID2: [
+            {id: '1', title: "bread", isDone: false},
+            {id: '2', title: "milk", isDone: true},
+            {id: '3', title: "tea", isDone: false}
+        ]
+    }
+
+    const endState = tasksReducer(startState, removeTaskAC("todolistID2", '2'))
+    expect(endState).toEqual({
+        todolistID1: [
+            {id: '1', title: "CSS", isDone: false},
+            {id: '2', title: "JS", isDone: true},
+            {id: '3', title: "React", isDone: false}
+        ],
+        todolistID2: [
+            {id: '1', title: "bread", isDone: false},
+            {id: '3', title: "tea", isDone: false}
+        ]
+    })
+})
