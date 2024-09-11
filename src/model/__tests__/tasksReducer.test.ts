b8a3b7df5018b1b845a44c247771429dec5bb862
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 18:59:56 +0300

    Add test for adding tasks to the correct array
    Included a new test case to ensure tasks are added to the correct todolist array. This helps verify that the addTaskAC action creator works as intended within the tasksReducer.

--- src/model/__tests__/tasksReducer.test.ts
+++ src/model/__tests__/tasksReducer.test.ts
@@ -1,7 +1,7 @@
 import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
-import {removeTaskAC, tasksReducer} from "model/tasksReducer/tasksReducer.ts";
+import {addTaskAC, removeTaskAC, tasksReducer} from "model/tasksReducer/tasksReducer.ts";
 
-test("correct task should be deleted from correct array", ()=> {
+test("correct task should be deleted from correct array", () => {
     const startState: TasksStateProps = {
         todolistID1: [
             {id: '1', title: "CSS", isDone: false},
@@ -27,4 +27,24 @@ test("correct task should be deleted from correct array", ()=> {
             {id: '3', title: "tea", isDone: false}
         ]
     })
+})
+test("correct task should be added to correct array", () => {
+    const startState: TasksStateProps = {
+        todolistID1: [
+            {id: '1', title: "CSS", isDone: false},
+            {id: '2', title: "JS", isDone: true},
+            {id: '3', title: "React", isDone: false}
+        ],
+        todolistID2: [
+            {id: '1', title: "bread", isDone: false},
+            {id: '2', title: "milk", isDone: true},
+            {id: '3', title: "tea", isDone: false}
+        ]
+    }
+
+    const endState = tasksReducer(startState, addTaskAC({todolistID: "todolistID2", title: "juice"}))
+    expect(endState["todolistID1"].length).toBe(3)
+    expect(endState["todolistID2"].length).toBe(4)
+    expect(endState["todolistID2"][0].id).toBeDefined()
+    expect(endState["todolistID2"][0].title).toBe("juice")
 })
