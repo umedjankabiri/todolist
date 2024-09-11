96d1298686c0fcccef24f07d4c8963b4ecbdcba3
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 17:32:09 +0300

    Add remove task functionality to tasksReducer
    Introduce a new tasksReducer function that handles the "REMOVE-TASK" action type. This function updates the state by removing a specified task from a todolist. Additionally, an action creator "removeTaskAC" is introduced to facilitate dispatching this action.

---
+++ src/model/tasksReducer/tasksReducer.ts
@@ -0,0 +1,18 @@
+import {TasksActionsProps} from "common/types/taskReducer/TasksActionsProps.ts";
+import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
+
+export const tasksReducer = (state: TasksStateProps, action: TasksActionsProps)=> {
+    const {todolistID, taskID} = action.payload
+    switch (action.type) {
+        case "REMOVE-TASK":
+            return {
+                ...state,
+                [todolistID]: state[todolistID].filter(task => task.id !== taskID)
+            }
+        default:
+            throw new Error(`Unknown action type ${action.type}`);
+    }
+}
+
+export const removeTaskAC = (todolistID: string, taskID: string) =>
+    ({type: "REMOVE-TASK", payload: {todolistID: todolistID, taskID: taskID}} as const)
