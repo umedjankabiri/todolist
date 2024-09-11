b7c8054f2ac9a0ce31c27c5a51061bdcf93b716d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:58:08 +0300

    Add test for adding new array when todolist is added
    Included a test to verify that a new array is properly added to the state when a new todolist is introduced. This ensures the tasks reducer handles the addition of new todolists correctly.

--- src/model/__tests__/tasksReducer.test.ts
+++ src/model/__tests__/tasksReducer.test.ts
@@ -6,6 +6,7 @@ import {
     removeTaskAC,
     tasksReducer
 } from "model/tasksReducer/tasksReducer.ts";
+import {addTodolistAC} from "model/todolistsReducer/todolistsReducer.ts";
 
 test("correct task should be deleted from correct array", () => {
     const startState: TasksStateProps = {
@@ -91,4 +92,29 @@ test('title of specified task should be changed', () => {
 
     expect(endState["todolistID1"][1].title).toBe("JS")
     expect(endState["todolistID2"][1].title).toBe("coffee")
+})
+test('new array should be added when new todolist is added', () => {
+    const startState: TasksStateProps = {
+        todolistId1: [
+            { id: '1', title: 'CSS', isDone: false },
+            { id: '2', title: 'JS', isDone: true },
+            { id: '3', title: 'React', isDone: false },
+        ],
+        todolistId2: [
+            { id: '1', title: 'bread', isDone: false },
+            { id: '2', title: 'milk', isDone: true },
+            { id: '3', title: 'tea', isDone: false },
+        ],
+    }
+
+    const endState = tasksReducer(startState, addTodolistAC('What to read'))
+
+    const keys = Object.keys(endState)
+    const newKey = keys.find(k => k !== 'todolistId1' && k !== 'todolistId2')
+    if (!newKey) {
+        throw Error('new key should be added')
+    }
+
+    expect(keys.length).toBe(3)
+    expect(endState[newKey]).toEqual([])
 })
