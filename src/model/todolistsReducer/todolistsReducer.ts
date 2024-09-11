f46bf994e7c59783577fd4db7293f332f8bddcfd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:57:26 +0300

    Refactor todolistsReducer to handle pre-generated IDs
    Replace in-place ID generation with pre-generated IDs passed through actions. This change ensures consistency in using IDs, improving the reliability of the state updates.

--- src/model/todolistsReducer/todolistsReducer.ts
+++ src/model/todolistsReducer/todolistsReducer.ts
@@ -17,7 +17,7 @@ export const todolistsReducer = (state: TodolistsProps[] = initialState, action:
             return state.filter(todolist => todolist.todolistID !== action.payload.todolistID)
         case "ADD-TODOLIST": {
             const newTodolist: TodolistsProps = {
-                todolistID: v1(),
+                todolistID: action.payload.todolistID,
                 title: action.payload.title,
                 filter: "All"
             }
@@ -41,7 +41,7 @@ export const todolistsReducer = (state: TodolistsProps[] = initialState, action:
 export const removeTodolistAC = (todolistID: string) =>
     ({type: "REMOVE-TODOLIST", payload: {todolistID: todolistID}}) as const
 export const addTodolistAC = (title: string) =>
-    ({type: "ADD-TODOLIST", payload: {title: title}}) as const
+    ({type: "ADD-TODOLIST", payload: {title: title, todolistID: v1()}}) as const
 export const changeTodolistTitleAC = (todolistID: string, title: string) =>
     ({type: "CHANGE-TODOLIST-TITLE", payload: {todolistID: todolistID, title: title}}) as const
 export const changeTodolistFilterAC = (todolistID: string, filter: FilterValueProps) =>
