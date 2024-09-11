5c3e2acc37a0443267fa347e282d56c340fc58a0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:30:49 +0300

    Add test for changeTaskStatus action in tasksReducer
    Extended tasksReducer test suite to include a new test for the changeTaskStatusAC action. This ensures that the status of a specified task can be correctly updated within the state.

--- src/model/__tests__/tasksReducer.test.ts
+++ src/model/__tests__/tasksReducer.test.ts
@@ -1,5 +1,5 @@
 import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
-import {addTaskAC, removeTaskAC, tasksReducer} from "model/tasksReducer/tasksReducer.ts";
+import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "model/tasksReducer/tasksReducer.ts";
 
 test("correct task should be deleted from correct array", () => {
     const startState: TasksStateProps = {
@@ -47,4 +47,23 @@ test("correct task should be added to correct array", () => {
     expect(endState["todolistID2"].length).toBe(4)
     expect(endState["todolistID2"][0].id).toBeDefined()
     expect(endState["todolistID2"][0].title).toBe("juice")
+})
+test('status of specified task should be changed', () => {
+    const startState: TasksStateProps = {
+        todolistID1: [
+            {id: '1', title: "CSS", isDone: false},
+            {id: '2', title: "JS", isDone: true},
+            {id: '3', title: "React", isDone: false}
+        ],
+        todolistID2: [
+            {id: '1', title: "bread", isDone: false},
+            {id: '2', title: "milk", isDone: true},
+            {id: '3', title: "tea", isDone: false}
+        ]
+    }
+
+    const endState = tasksReducer(startState, changeTaskStatusAC({todolistID: 'todolistID2', taskID: '2', isDone: false}))
+
+    expect(endState["todolistID1"][1].isDone).toBe(true)
+    expect(endState["todolistID2"][1].isDone).toBe(false)
 })
