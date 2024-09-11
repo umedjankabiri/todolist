1cdc3c6d671f1ebd87a4d43638162b33dbfcacb8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 20:02:31 +0300

    Add test for validating todolist and task ID synchronization
    This test ensures the IDs generated for todolists and tasks are synchronized when a new todolist is added. This is achieved by comparing the IDs in the state after the addition action is dispatched.

---
+++ src/model/__tests__/todolistsTasksReducer.test.ts
@@ -0,0 +1,21 @@
+import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
+import {TodolistsProps} from "common/types/Todolists/TodolistsProps.ts";
+import {addTodolistAC, todolistsReducer} from "model/todolistsReducer/todolistsReducer.ts";
+import {tasksReducer} from "model/tasksReducer/tasksReducer.ts";
+
+test('ids should be equals', () => {
+    const startTasksState: TasksStateProps = {}
+    const startTodolistsState: TodolistsProps[] = []
+
+    const action = addTodolistAC('What to read')
+
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodolistsState = todolistsReducer(startTodolistsState, action)
+
+    const keys = Object.keys(endTasksState)
+    const idFromTasks = keys[0]
+    const idFromTodolists = endTodolistsState[0].todolistID
+
+    expect(idFromTasks).toBe(action.payload.todolistID)
+    expect(idFromTodolists).toBe(action.payload.todolistID)
+})
