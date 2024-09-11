43af3d219eca5e082258839c7221b8998d1dbc96
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:31:28 +0300

    Add ChangeTaskTitle action type to TasksActionsProps
    Included the changeTaskTitleAC in the import statement and defined the corresponding ChangeTaskTitleProps type. Updated the TasksActionsProps type to incorporate ChangeTaskTitleProps.

--- src/common/types/taskReducer/TasksActionsProps.ts
+++ src/common/types/taskReducer/TasksActionsProps.ts
@@ -1,8 +1,10 @@
-import {addTaskAC, changeTaskStatusAC, removeTaskAC} from "model/tasksReducer/tasksReducer";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC} from "model/tasksReducer/tasksReducer";
 
 export type RemoveTaskActionProps = ReturnType<typeof removeTaskAC>
 export type AddTaskActionProps = ReturnType<typeof addTaskAC>
 export type ChangeTaskStatusProps = ReturnType<typeof changeTaskStatusAC>
+export type ChangeTaskTitleProps = ReturnType<typeof changeTaskTitleAC>
 export type TasksActionsProps = RemoveTaskActionProps
     | AddTaskActionProps
     | ChangeTaskStatusProps
+    | ChangeTaskTitleProps
