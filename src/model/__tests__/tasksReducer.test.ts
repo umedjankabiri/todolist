fa22473dfaa6f2f495eea18655e14de7be9acb44
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:31:54 +0300

    Add test for changing task title in tasksReducer
    Introduce a new test case to verify that the task title changes correctly in the tasksReducer. This ensures proper functionality for the `changeTaskTitleAC` action.

--- src/model/__tests__/tasksReducer.test.ts
+++ src/model/__tests__/tasksReducer.test.ts
@@ -1,5 +1,11 @@
 import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
-import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "model/tasksReducer/tasksReducer.ts";
+import {
+    addTaskAC,
+    changeTaskStatusAC,
+    changeTaskTitleAC,
+    removeTaskAC,
+    tasksReducer
+} from "model/tasksReducer/tasksReducer.ts";
 
 test("correct task should be deleted from correct array", () => {
     const startState: TasksStateProps = {
@@ -66,4 +72,23 @@ test('status of specified task should be changed', () => {
 
     expect(endState["todolistID1"][1].isDone).toBe(true)
     expect(endState["todolistID2"][1].isDone).toBe(false)
+})
+test('title of specified task should be changed', () => {
+    const startState: TasksStateProps = {
+        todolistID1: [
+            {id: '1', title: "CSS", isDone: false},
+            {id: '2', title: "JS", isDone: true},
+            {id: '3', title: "React", isDone: false}
+        ],
+        todolistID2: [
+            {id: '1', title: "bread", isDone: false},
+            {id: '2', title: "milk", isDone: true},
+            {id: '3', title: "tea", isDone: false}
+        ]
+    }
+
+    const endState = tasksReducer(startState, changeTaskTitleAC({todolistID: 'todolistID2', taskID: '2', title: "coffee"}))
+
+    expect(endState["todolistID1"][1].title).toBe("JS")
+    expect(endState["todolistID2"][1].title).toBe("coffee")
 })
