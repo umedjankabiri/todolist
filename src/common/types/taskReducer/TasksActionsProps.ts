f365e27c5dec65bade27186aa494a305bee60ec3
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:30:33 +0300

    Add changeTaskStatusAC to TasksActionsProps
    This commit includes the import and type definition for the changeTaskStatusAC action in the TasksActionsProps. This update allows the handling of task status changes alongside existing task addition and removal actions.

--- src/common/types/taskReducer/TasksActionsProps.ts
+++ src/common/types/taskReducer/TasksActionsProps.ts
@@ -1,6 +1,8 @@
-import {addTaskAC, removeTaskAC} from "model/tasksReducer/tasksReducer";
-
-export type TasksActionsProps = RemoveTaskActionProps | AddTaskActionProps
+import {addTaskAC, changeTaskStatusAC, removeTaskAC} from "model/tasksReducer/tasksReducer";
 
 export type RemoveTaskActionProps = ReturnType<typeof removeTaskAC>
 export type AddTaskActionProps = ReturnType<typeof addTaskAC>
+export type ChangeTaskStatusProps = ReturnType<typeof changeTaskStatusAC>
+export type TasksActionsProps = RemoveTaskActionProps
+    | AddTaskActionProps
+    | ChangeTaskStatusProps
