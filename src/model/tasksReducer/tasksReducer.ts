cafd5595abd8048f40280481ebae95a1636bc971
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-11 19:00:04 +0300

    Add support for adding tasks to tasksReducer
    Implemented a new case for the ADD-TASK action type in the tasksReducer, allowing tasks to be added with a unique ID, title, and default completion status. Also refactored error handling for unknown action types.

--- src/model/tasksReducer/tasksReducer.ts
+++ src/model/tasksReducer/tasksReducer.ts
@@ -1,18 +1,31 @@
-import {TasksActionsProps} from "common/types/taskReducer/TasksActionsProps.ts";
 import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
+import {TasksActionsProps} from "common/types/taskReducer/TasksActionsProps.ts";
+import {v1} from "uuid";
 
-export const tasksReducer = (state: TasksStateProps, action: TasksActionsProps)=> {
-    const {todolistID, taskID} = action.payload
+export const tasksReducer = (state: TasksStateProps, action: TasksActionsProps): TasksStateProps => {
     switch (action.type) {
-        case "REMOVE-TASK":
+        case "REMOVE-TASK": {
+            const {todolistID, taskID} = action.payload
             return {
                 ...state,
                 [todolistID]: state[todolistID].filter(task => task.id !== taskID)
             }
-        default:
-            throw new Error(`Unknown action type ${action.type}`);
+        }
+        case "ADD-TASK": {
+            const {todolistID, title} = action.payload
+            return {
+                ...state,
+                [todolistID]: [{id: v1(), title: title, isDone: false}, ...state[todolistID]]
+            }
+        }
+        default: {
+            const unknownAction = action as unknown
+            throw new Error(`Unknown action type: ${(unknownAction as {type: string}).type}`);
+        }
     }
 }
 
-export const removeTaskAC = (payload: {todolistID: string, taskID: string}) =>
+export const removeTaskAC = (payload: { todolistID: string, taskID: string }) =>
     ({type: "REMOVE-TASK", payload}) as const
+export const addTaskAC = (payload: { todolistID: string, title: string }) =>
+    ({type: "ADD-TASK", payload}) as const
