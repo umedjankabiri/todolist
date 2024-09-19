d5ea68aa238505b1751d27bf5d4481278f134599
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-20 01:41:17 +0300

    Refactor AppWithRedux component to use Redux hooks
    Replaced useReducer with useSelector and useDispatch for better integration with the Redux store. This simplifies the state management by leveraging Redux hooks and removing redundant initial state definitions.

--- src/App/AppWithRedux.tsx
+++ src/App/AppWithRedux.tsx
@@ -1,20 +1,11 @@
 import 'App/App.css'
 import {Todolist} from 'common/components/Todolist/Todolist.tsx';
-import {useReducer, useState} from "react";
+import {useState} from "react";
 import {FilterValueProps} from "common/types/Tasks/FilterValueProps.ts";
-import {v1} from "uuid";
 import {TodolistsProps} from "common/types/Todolists/TodolistsProps.ts";
 import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
 import {AddItemForm} from "common/components/AddItemForm/AddItemForm.tsx";
-import {
-    AppBar,
-    Container,
-    CssBaseline,
-    Paper,
-    Switch,
-    Toolbar,
-    Typography
-} from "@mui/material";
+import {AppBar, Container, CssBaseline, Paper, Switch, Toolbar, Typography} from "@mui/material";
 import {createTheme, ThemeProvider} from "@mui/material/styles"
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu"
@@ -23,43 +14,19 @@ import {MenuButton} from "common/components/MenuButtons/MenuButton.tsx";
 import {ThemeMode} from "common/types/ThemeMode/ThemeModeProps.ts";
 import {
     addTodolistAC,
-    changeTodolistFilterAC, changeTodolistTitleAC,
-    removeTodolistAC,
-    todolistsReducer
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    removeTodolistAC
 } from "model/todolistsReducer/todolistsReducer.ts";
-import {
-    addTaskAC,
-    changeTaskStatusAC,
-    changeTaskTitleAC,
-    removeTaskAC,
-    tasksReducer
-} from "model/tasksReducer/tasksReducer.ts";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC} from "model/tasksReducer/tasksReducer.ts";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "App/store.ts";
 
 // Business Logic Layer (BLL)
-export let todolistID1 = v1()
-export let todolistID2 = v1()
-export const todolistsInitialState: TodolistsProps[] = [
-    {todolistID: todolistID1, title: "What to read", filter: "All"},
-    {todolistID: todolistID2, title: "What to buy", filter: "All"},
-]
-export const tasksInitialState: TasksStateProps = {
-    [todolistID1]: [
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: false},
-    ],
-    [todolistID2]: [
-        {id: v1(), title: 'macbook pro 16 M3 MAX', isDone: true},
-        {id: v1(), title: 'iphone 15 512 pro MAX', isDone: true},
-        {id: v1(), title: 'ipad', isDone: false},
-        {id: v1(), title: 'apple watch ultra', isDone: false},
-        {id: v1(), title: 'airpods', isDone: false},
-    ]
-}
-
 function AppWithRedux() {
-    const [todolists, dispatchTodolists] = useReducer(todolistsReducer, todolistsInitialState)
-    const [tasks, dispatchTasks] = useReducer(tasksReducer, tasksInitialState);
+    const todolists = useSelector<RootState, TodolistsProps[]>(state => state.todolists)
+    const tasks = useSelector<RootState, TasksStateProps>(state => state.tasks)
+    const dispatch = useDispatch()
     const [themeMode, setThemeMode] = useState<ThemeMode>("light")
 
     const theme = createTheme({
@@ -75,28 +42,28 @@ function AppWithRedux() {
 
     const changeThemeMode = ()=> setThemeMode(themeMode === "light" ? "dark" : "light")
     const removedTask = (todolistID: string, taskID: string) =>
-        dispatchTasks(removeTaskAC({todolistID: todolistID, taskID: taskID}));
+        dispatch(removeTaskAC({todolistID: todolistID, taskID: taskID}));
     const addedTask = (todolistID: string, title: string) =>
-        dispatchTasks(addTaskAC({todolistID: todolistID, title: title}));
+        dispatch(addTaskAC({todolistID: todolistID, title: title}));
     const changedTaskStatus = (todolistID: string, taskID: string, taskStatus: boolean) =>
-        dispatchTasks(changeTaskStatusAC({todolistID: todolistID, taskID: taskID, isDone: taskStatus}));
+        dispatch(changeTaskStatusAC({todolistID: todolistID, taskID: taskID, isDone: taskStatus}));
     const changedTodolistFilter = (todolistID: string, filterValue: FilterValueProps) =>
-        dispatchTodolists(changeTodolistFilterAC(todolistID, filterValue))
+        dispatch(changeTodolistFilterAC(todolistID, filterValue))
 
     const removedTodolist = (todolistID: string) => {
         const action = removeTodolistAC(todolistID)
-        dispatchTodolists(action);
-        dispatchTasks(action)
+        dispatch(action);
+        dispatch(action)
     }
     const AddTodolist = (title: string) => {
         const action = addTodolistAC(title)
-        dispatchTodolists(action);
-        dispatchTasks(action)
+        dispatch(action);
+        dispatch(action)
     }
     const changeTaskTitleHandler = (todolistID: string, taskID: string, title: string) =>
-        dispatchTasks(changeTaskTitleAC({todolistID: todolistID, taskID: taskID, title: title}));
+        dispatch(changeTaskTitleAC({todolistID: todolistID, taskID: taskID, title: title}));
     const changeTodolistTitleHandler = (todolistID: string, title: string) =>
-        dispatchTodolists(changeTodolistTitleAC(todolistID, title))
+        dispatch(changeTodolistTitleAC(todolistID, title))
 
     const buttonsBackgroundColor = themeMode === "light" ? theme.palette.primary.light : theme.palette.primary.dark
 
