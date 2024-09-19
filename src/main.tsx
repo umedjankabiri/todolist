7a7eb9d60d5496ce802aa910f360f0fc333d27b8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-20 01:15:53 +0300

    Add Redux Provider to main.tsx
    Wrapped AppWithRedux with Redux Provider to integrate the Redux store. This ensures that the entire application has access to the centralized state managed by Redux.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import AppWithRedux from "App/AppWithRedux.tsx";
+import {Provider} from "react-redux";
+import {store} from "App/store.ts";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppWithRedux />
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <AppWithRedux/>
+        </Provider>
+    </StrictMode>,
 )
