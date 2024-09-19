2a8ee58bc82ef9f50111ee004919bc4b667cf241
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-20 02:39:29 +0300

    Refactor reducer action creators for improved readability
    Updated the action creator functions to use object destructuring for payload parameters. This simplifies the code and makes action parameter management more consistent. Explicitly specifying the return type for the reducer function enhances type safety.

--- src/model/todolistsReducer/todolistsReducer.ts
+++ src/model/todolistsReducer/todolistsReducer.ts
@@ -5,7 +5,7 @@ import {TodolistsActionsProps} from "common/types/TodolistsReducer/TodolistsActi
 
 const initialTodolistsState: TodolistsProps[] = []
 
-export const todolistsReducer = (state: TodolistsProps[] = initialTodolistsState, action: TodolistsActionsProps) => {
+export const todolistsReducer = (state: TodolistsProps[] = initialTodolistsState, action: TodolistsActionsProps): TodolistsProps[] => {
     switch (action.type) {
         case "REMOVE-TODOLIST":
             return state.filter(todolist => todolist.todolistID !== action.payload.todolistID)
@@ -36,7 +36,7 @@ export const removeTodolistAC = (todolistID: string) =>
     ({type: "REMOVE-TODOLIST", payload: {todolistID: todolistID}}) as const
 export const addTodolistAC = (title: string) =>
     ({type: "ADD-TODOLIST", payload: {title: title, todolistID: v1()}}) as const
-export const changeTodolistTitleAC = (todolistID: string, title: string) =>
-    ({type: "CHANGE-TODOLIST-TITLE", payload: {todolistID: todolistID, title: title}}) as const
-export const changeTodolistFilterAC = (todolistID: string, filter: FilterValueProps) =>
-    ({type: "CHANGE-TODOLIST-FILTER", payload: {todolistID: todolistID, filter: filter}}) as const
+export const changeTodolistTitleAC = (payload: {todolistID: string, title: string}) =>
+    ({type: "CHANGE-TODOLIST-TITLE", payload}) as const
+export const changeTodolistFilterAC = (payload: {todolistID: string, filter: FilterValueProps}) =>
+    ({type: "CHANGE-TODOLIST-FILTER", payload}) as const
