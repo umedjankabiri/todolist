defa86c36a10e0a108f838f22883bb1a7c013f8e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-20 02:07:28 +0300

    Fix typo in root reducer name
    Corrected the spelling of `rootReducers` in the store setup. This change ensures the correct function is used, preventing potential runtime errors.

--- src/App/store.ts
+++ src/App/store.ts
@@ -2,11 +2,11 @@ import { combineReducers, legacy_createStore } from "@reduxjs/toolkit";
 import {todolistsReducer} from "model/todolistsReducer/todolistsReducer.ts";
 import {tasksReducer} from "model/tasksReducer/tasksReducer.ts";
 
-const rooReducers = combineReducers({
+const rootReducers = combineReducers({
     todolists: todolistsReducer,
     tasks: tasksReducer
 })
-export const store = legacy_createStore(rooReducers)
+export const store = legacy_createStore(rootReducers)
 export type RootState = ReturnType<typeof store.getState>
 
 // @ts-ignore
