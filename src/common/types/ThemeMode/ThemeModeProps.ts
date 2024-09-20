d25d2bd167c9066c6dad18b844637ffee8624594
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 00:36:30 +0300

    Add type definitions for theme actions and state
    This commit introduces type definitions for the initial state and theme action properties. These enhancements ensure better type safety and code clarity when handling theme mode changes.

--- src/common/types/ThemeMode/ThemeModeProps.ts
+++ src/common/types/ThemeMode/ThemeModeProps.ts
@@ -1,1 +1,6 @@
+import {changeThemeModeAC, initialState} from "model/themeReducer/themeReducer.ts";
+
 export type ThemeMode = "dark" | "light"
+export type InitialState = typeof initialState
+export type changeThemeModeActionProps = ReturnType<typeof changeThemeModeAC>;
+export type ActionProps = changeThemeModeActionProps
