8f3cce294c4a6728e97341010da764a99e68846a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 02:04:00 +0300

    Refactor App with simplified structure and modular components
    Reorganize the `App` component by extracting and modularizing header and main content into separate components. Removed redundant code and streamlined the theme toggle implementation, focusing on a cleaner and more maintainable structure.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,121 +1,23 @@
 import 'App/App.css'
-import {Todolist} from 'common/components/Todolist/Todolist.tsx';
-import {FilterValueProps} from "common/types/Tasks/FilterValueProps.ts";
-import {TodolistsProps} from "common/types/Todolists/TodolistsProps.ts";
-import {TasksStateProps} from "common/types/Tasks/TasksStateProps.ts";
-import {AddItemForm} from "common/components/AddItemForm/AddItemForm.tsx";
-import {AppBar, Container, CssBaseline, Paper, Switch, Toolbar, Typography} from "@mui/material";
-import {ThemeProvider} from "@mui/material/styles"
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu"
-import Grid from '@mui/material/Grid2'
-import {MenuButton} from "common/components/MenuButtons/MenuButton.tsx";
-import {ThemeMode} from "common/types/ThemeMode/ThemeModeProps.ts";
-import {
-    addTodolistAC,
-    changeTodolistFilterAC,
-    changeTodolistTitleAC,
-    removeTodolistAC
-} from "model/todolistsReducer/todolistsReducer.ts";
-import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC} from "model/tasksReducer/tasksReducer.ts";
-import {useDispatch, useSelector} from "react-redux";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {useSelector} from "react-redux";
 import {RootState} from "App/store.ts";
+import {ThemeMode} from "common/types/ThemeMode/ThemeModeProps.ts";
+import {Header} from "common/components/Header/Header.tsx";
+import {Main} from "common/components/Main/Main.tsx";
 import {toggleTheme} from "common/utils/toggleTheme.ts";
-import {changeThemeModeAC} from "model/themeReducer/themeReducer.ts";
 
 // Business Logic Layer (BLL)
-function App() {
-    const todolists = useSelector<RootState, TodolistsProps[]>(state => state.todolists)
-    const tasks = useSelector<RootState, TasksStateProps>(state => state.tasks)
+export const App = () => {
     const themeMode = useSelector<RootState, ThemeMode>(state => state.themes?.themeMode)
-    const dispatch = useDispatch()
-
-    // CRUD for todolists
-    const removedTodolist = (todolistID: string) => dispatch(removeTodolistAC(todolistID));
-    const AddTodolist = (title: string) => dispatch(addTodolistAC(title));
-    const changeTodolistTitle = (todolistID: string, title: string) =>
-        dispatch(changeTodolistTitleAC({todolistID: todolistID, title: title}))
-    const changedTodolistFilter = (todolistID: string, filterValue: FilterValueProps) =>
-        dispatch(changeTodolistFilterAC({todolistID: todolistID, filter: filterValue}))
-
-    // CRUD for tasks
-    const removedTask = (todolistID: string, taskID: string) =>
-        dispatch(removeTaskAC({todolistID: todolistID, taskID: taskID}));
-    const addedTask = (todolistID: string, title: string) =>
-        dispatch(addTaskAC({todolistID: todolistID, title: title}));
-    const changedTaskStatus = (todolistID: string, taskID: string, taskStatus: boolean) =>
-        dispatch(changeTaskStatusAC({todolistID: todolistID, taskID: taskID, isDone: taskStatus}));
-    const changeTaskTitle = (todolistID: string, taskID: string, title: string) =>
-        dispatch(changeTaskTitleAC({todolistID: todolistID, taskID: taskID, title: title}));
-
-    // changing theme of todolist app
     const theme = toggleTheme(themeMode)
-    const changeThemeMode = () => dispatch(changeThemeModeAC(themeMode === "light" ? "dark" : "light"))
-    const buttonsBackgroundColor = themeMode === "light" ? theme.palette.primary.light : theme.palette.primary.dark
 
-    // mapped todolists for render
-    const filteredTodolists = todolists.map(todolist => {
-        let filteredTasks = tasks[todolist.todolistID]
-        todolist.filter === "Active" && (filteredTasks = filteredTasks.filter(task => !task.isDone))
-        todolist.filter === "Completed" && (filteredTasks = filteredTasks.filter(task => task.isDone))
-
-        // layout of todolists
-        return (
-            <Grid key={todolist.todolistID}>
-                <Paper sx={{p: "0 20px 20px 20px", boxShadow: `4px 4px 10px 0.5px ${"#504e4e"}`}}>
-                    <Todolist
-                        key={todolist.todolistID}
-                        todolistID={todolist.todolistID}
-                        title={todolist.title}
-                        tasks={filteredTasks}
-                        removeTodolist={removedTodolist}
-                        removeTask={removedTask}
-                        changeFilter={changedTodolistFilter}
-                        addTask={addedTask}
-                        changeTaskStatus={changedTaskStatus}
-                        filter={todolist.filter}
-                        changeTaskTitle={changeTaskTitle}
-                        changeTodolistTitle={changeTodolistTitle}
-                    />
-                </Paper>
-            </Grid>
-        )
-    })
-
-    // layout
+    // main layout
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <AppBar position={"static"} sx={{mb: "30px"}}>
-                <Toolbar>
-                    <IconButton color="inherit">
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" component="span" sx={{flexGrow: 1, marginLeft: "10px"}}>
-                        Todolist App
-                    </Typography>
-                    <Typography variant={"h6"}>
-                        {themeMode === "light" ? "to dark: " : "to light: "}
-                    </Typography>
-                    <Switch color={"default"} onChange={changeThemeMode}/>
-                    <MenuButton background={buttonsBackgroundColor}>login</MenuButton>
-                    <MenuButton background={buttonsBackgroundColor}>logout</MenuButton>
-                    <MenuButton background={buttonsBackgroundColor}>faq</MenuButton>
-                </Toolbar>
-            </AppBar>
-            <Container fixed>
-                <Grid container sx={{marginBottom: "30px"}}>
-                    <Paper sx={{p: "0 20px 20px 20px", boxShadow: `4px 4px 10px 0.5px ${"#504e4e"}`}}>
-                        <h3>Create new todolist</h3>
-                        <AddItemForm addItem={AddTodolist}/>
-                    </Paper>
-                </Grid>
-                <Grid container spacing={4}>
-                    {filteredTodolists}
-                </Grid>
-            </Container>
+            <Header/>
+            <Main/>
         </ThemeProvider>
     );
 }
-
-export default App
