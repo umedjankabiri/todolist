487d7b5bf973dc7efb12ca7a81cec3a1fd4e01de
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 00:36:20 +0300

    Add themeReducer to rootReducers in store.ts
    This commit integrates the themeReducer into the rootReducers, enabling state management for themes within the store. It modifies the combineReducers function from "@reduxjs/toolkit" to include the new reducer.

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, legacy_createStore } from "@reduxjs/toolkit";
 import {todolistsReducer} from "model/todolistsReducer/todolistsReducer.ts";
 import {tasksReducer} from "model/tasksReducer/tasksReducer.ts";
+import {themeReducer} from "model/themeReducer/themeReducer.ts";
 
 const rootReducers = combineReducers({
     todolists: todolistsReducer,
-    tasks: tasksReducer
+    tasks: tasksReducer,
+    themes: themeReducer
 })
 export const store = legacy_createStore(rootReducers)
 export type RootState = ReturnType<typeof store.getState>
