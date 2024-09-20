00fd16cc88a3e093d00d7cd4ce434e92bc78e3f9
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 00:35:50 +0300

    Add theme toggling utility
    Introduce a `toggleTheme` function to dynamically switch between light and dark modes. This function leverages Material-UI's `createTheme` and adjusts the primary palette colors based on the selected theme mode.

---
+++ src/common/utils/toggleTheme.ts
@@ -0,0 +1,15 @@
+import {createTheme} from "@mui/material/styles";
+import {ThemeMode} from "common/types/ThemeMode/ThemeModeProps.ts";
+
+export const toggleTheme = (themeMode: ThemeMode) => {
+    return createTheme({
+        palette: {
+            mode: themeMode === "light" ? "light" : "dark",
+            primary: {
+                main: "#087EA4",
+                light: "#0993cb",
+                dark: "#033844",
+            }
+        }
+    })
+}
