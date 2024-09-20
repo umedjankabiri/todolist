e0a0ae6bf74a36901ccda149f04df3815841181f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 00:35:59 +0300

    Add themeReducer for managing theme state
    This commit introduces the themeReducer in a new file to handle theme state management. It includes an initial state definition, a reducer function to switch themes, and an action creator for changing the theme mode.

---
+++ src/model/themeReducer/themeReducer.ts
@@ -0,0 +1,20 @@
+import {ActionProps, InitialState, ThemeMode} from "common/types/ThemeMode/ThemeModeProps.ts";
+
+export const initialState = {
+    themeMode: "light" as ThemeMode,
+}
+
+export const themeReducer = (state: InitialState = initialState, action: ActionProps) => {
+    switch (action.type) {
+        case"CHANGE_THEME":
+            return {
+                ...state,
+                themeMode: action.themeMode
+            }
+        default:
+            return state
+    }
+}
+
+export const changeThemeModeAC = (themeMode: ThemeMode) =>
+    ({type: "CHANGE_THEME", themeMode: themeMode})
