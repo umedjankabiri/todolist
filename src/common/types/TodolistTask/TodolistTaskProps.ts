50573d002481cb1016bfaa5837ed472a8200fd73
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 23:02:18 +0300

    Add TodolistTaskProps type definition
    Introduce `TodolistTaskProps` type to encapsulate properties required for both a task and its associated todolist. This ensures better type safety and consistency in handling todolist and task relationships.

---
+++ src/common/types/TodolistTask/TodolistTaskProps.ts
@@ -0,0 +1,7 @@
+import {TaskProps} from "common/types/Tasks/TaskProps.ts";
+import {TodolistsProps} from "common/types/Todolists/TodolistsProps.ts";
+
+export type TodolistTaskProps = {
+    todolist: TodolistsProps
+    task: TaskProps
+}
