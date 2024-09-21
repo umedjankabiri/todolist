ed89791914f109d0ee8a2a51128f31e5973d9128
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 23:20:42 +0300

    Refactor folder structure for Todolist components
    Moved Todolist-related components into a more organized directory structure under 'Todolist'. This change improves the maintainability and readability of the code by grouping related components together. All import paths have been updated accordingly.

--- src/common/components/Todolists/Todolist/Todolist.tsx
+++ src/common/components/Todolists/Todolist/Todolist.tsx
@@ -1,11 +1,11 @@
 import {TodolistProps} from "common/types/Todolists/Todolist/TodolistProps.ts";
 import {FC} from "react";
 import {AddItemForm} from "common/components/AddItemForm/AddItemForm.tsx";
-import {FilterButtons} from "common/components/Todolists/Tasks/FilterButtons/FilterButtons.tsx";
+import {FilterButtons} from "common/components/Todolists/Todolist/FilterButtons/FilterButtons.tsx";
 import {useDispatch} from "react-redux";
 import {addTaskAC} from "model/tasksReducer/tasksReducer.ts";
-import {TodolistTitle} from "common/components/Todolists/TodolistTitle/TodolistTitle.tsx";
-import {Tasks} from "common/components/Todolists/Tasks/Tasks.tsx";
+import {TodolistTitle} from "common/components/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx";
+import {Tasks} from "common/components/Todolists/Todolist/Tasks/Tasks.tsx";
 
 export const Todolist: FC<TodolistProps> = ({todolist}) => {
     const dispatch = useDispatch()
