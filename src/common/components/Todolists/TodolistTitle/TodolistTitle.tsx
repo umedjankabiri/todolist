a550b31d939d3f580cca5769b3c262d3aa1216bd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 22:10:35 +0300

    Add TodolistTitle component to manage todolist title
    This new component allows users to edit and delete todolists. It uses EditableSpan for title modifications and IconButton for deletion. Integrates with Redux actions for state management.

---
+++ src/common/components/Todolists/TodolistTitle/TodolistTitle.tsx
@@ -0,0 +1,27 @@
+import {FC} from "react";
+import {TodolistProps} from "common/types/Todolist/TodolistProps.ts";
+import {EditableSpan} from "common/components/EditableSpan/EditableSpan.tsx";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {useDispatch} from "react-redux";
+import {changeTodolistTitleAC, removeTodolistAC} from "model/todolistsReducer/todolistsReducer.ts";
+
+export const TodolistTitle: FC<TodolistProps> = ({todolist}) => {
+    const dispatch = useDispatch();
+
+    const {todolistID, title} = todolist
+    const removeTodolistHandler = () => dispatch(removeTodolistAC(todolistID))
+    const changeTodolistTitleHandler = (title: string) =>
+        dispatch(changeTodolistTitleAC({todolistID: todolistID, title: title}))
+
+    return (
+        <div className={"todolist-title-container"}>
+            <h3>
+                <EditableSpan title={title} onChangeTitle={changeTodolistTitleHandler}/>
+            </h3>
+            <IconButton onClick={removeTodolistHandler}>
+                <DeleteIcon/>
+            </IconButton>
+        </div>
+    );
+};
