a87b72da22ac50971adb0aea12417f3b51a852ac
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-21 22:09:34 +0300

    Add FilterButtons component to manage task filters.
    This new component provides UI buttons for filtering tasks by their status: All, Active, and Completed. It utilizes Redux to dispatch filter change actions and updates the task list accordingly. The component is styled using MUI's Box and Button elements for a cohesive design.

---
+++ src/common/components/Tasks/FilterButtons/FilterButtons.tsx
@@ -0,0 +1,36 @@
+import {FC} from "react";
+import {Box} from "@mui/material";
+import {filterButtonsContainerSX} from "common/types/Todolist/Todolist.styles.ts";
+import Button from "@mui/material/Button";
+import {FilterValueProps} from "common/types/Tasks/FilterValueProps.ts";
+import {useDispatch} from "react-redux";
+import {changeTodolistFilterAC} from "model/todolistsReducer/todolistsReducer.ts";
+import {TodolistProps} from "common/types/Todolist/TodolistProps.ts";
+
+export const FilterButtons: FC<TodolistProps> = ({todolist}) => {
+    const dispatch = useDispatch()
+
+    const {todolistID, filter} = todolist
+    const changeFilterHandler = (filter: FilterValueProps) =>
+        dispatch(changeTodolistFilterAC({todolistID: todolistID, filter: filter}))
+
+    return (
+        <Box sx={filterButtonsContainerSX}>
+            <Button
+                variant={filter === "All" ? "outlined" : "text"}
+                color={"inherit"}
+                onClick={() => changeFilterHandler("All")}
+            >All</Button>
+            <Button
+                variant={filter === "Active" ? "outlined" : "text"}
+                color={"inherit"}
+                onClick={() => changeFilterHandler("Active")}
+            >Active</Button>
+            <Button
+                variant={filter === "Completed" ? "outlined" : "text"}
+                color={"inherit"}
+                onClick={() => changeFilterHandler("Completed")}
+            >Completed</Button>
+        </Box>
+    );
+};
