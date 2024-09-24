bc51c311954d7a159ccad07d30993e4cffb3ae21
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-24 23:54:24 +0300

    Add AppHttpRequests component for managing tasks
    Introduced a new AppHttpRequests component that facilitates creating, updating, and removing to-do lists and tasks. This component employs MUI's Checkbox and custom AddItemForm and EditableSpan components. Styles for to-do lists are also included.

---
+++ src/App/AppHttpRequest.tsx
@@ -0,0 +1,92 @@
+import Checkbox from '@mui/material/Checkbox'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { AddItemForm } from '../common/components/AddItemForm/AddItemForm'
+import { EditableSpan } from '../common/components/EditableSpan/EditableSpan'
+
+export const AppHttpRequests = () => {
+    const [todolists, setTodolists] = useState<any>([])
+    const [tasks, setTasks] = useState<any>({})
+
+    useEffect(() => {
+        // get todolists
+    }, [])
+
+    const createTodolistHandler = (title: string) => {
+        // create todolist
+    }
+
+    const removeTodolistHandler = (id: string) => {
+        // remove todolist
+    }
+
+    const updateTodolistHandler = (id: string, title: string) => {
+        // update todolist title
+    }
+
+    const createTaskHandler = (title: string, todolistId: string) => {
+        // create task
+    }
+
+    const removeTaskHandler = (taskId: string, todolistId: string) => {
+        // remove task
+    }
+
+    const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>, task: any) => {
+        // update task status
+    }
+
+    const changeTaskTitleHandler = (title: string, task: any) => {
+        // update task title
+    }
+
+    return (
+        <div style={{ margin: '20px' }}>
+            <AddItemForm addItem={createTodolistHandler} />
+
+            {/* Todolists */}
+            {todolists.map((tl: any) => {
+                return (
+                    <div key={tl.id} style={todolist}>
+                        <div>
+                            <EditableSpan
+                                title={tl.title}
+                                onChangeTitle={(title: string) => updateTodolistHandler(tl.id, title)}
+                            />
+                            <button onClick={() => removeTodolistHandler(tl.id)}>x</button>
+                        </div>
+                        <AddItemForm addItem={title => createTaskHandler(title, tl.id)} />
+
+                        {/* Tasks */}
+                        {!!tasks[tl.id] &&
+                            tasks[tl.id].map((task: any) => {
+                                return (
+                                    <div key={task.id}>
+                                        <Checkbox
+                                            checked={task.isDone}
+                                            onChange={e => changeTaskStatusHandler(e, task)}
+                                        />
+                                        <EditableSpan
+                                            title={task.title}
+                                            onChangeTitle={title => changeTaskTitleHandler(title, task)}
+                                        />
+                                        <button onClick={() => removeTaskHandler(task.id, tl.id)}>x</button>
+                                    </div>
+                                )
+                            })}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+// Styles
+const todolist: React.CSSProperties = {
+    border: '1px solid black',
+    margin: '20px 0',
+    padding: '10px',
+    width: '300px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+}
