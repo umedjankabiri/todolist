e50cd0d20d84389570306c016a2d4cf735d87446
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-25 03:42:27 +0300

    Refactor HTTP request types and handlers in AppHttpRequest
    Updated state type for `todolists` to `Todolist[]` and added specific response types for create, delete, and update HTTP requests. Refactored request handlers to include appropriate logging and filter logic for better state management.

--- src/App/AppHttpRequest.tsx
+++ src/App/AppHttpRequest.tsx
@@ -5,7 +5,7 @@ import {EditableSpan} from '../common/components/EditableSpan/EditableSpan'
 import axios from 'axios'
 
 export const AppHttpRequests = () => {
-    const [todolists, setTodolists] = useState<any>([])
+    const [todolists, setTodolists] = useState<Todolist[]>([])
     const [tasks, setTasks] = useState<any>({})
 
     useEffect(() => {
@@ -20,18 +20,37 @@ export const AppHttpRequests = () => {
     }, [])
 
     const createTodolistHandler = (title: string) => {
-        axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists",
-            {title: title}, {headers: {
-                Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
+        axios.post<CreateDeleteTodolistResponse>("https://social-network.samuraijs.com/api/1.1/todo-lists",
+            {title: title}, {
+                headers: {
+                    Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
                     "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
-            }})
-            .then((response) => {
-                setTodolists([response.data.data.item, ...todolists])
-            })
+                }
+            }).then((response) => {
+            console.log(response.data)
+            setTodolists([response.data.data.item, ...todolists])
+        })
     }
 
-    const removeTodolistHandler = (id: string) => {
-        // remove todolist
+    const removeTodolistHandler = (todolistID: string) => {
+        axios.delete<DeleteTodolistResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, {
+            headers: {
+                Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
+                "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
+            }
+        }).then((response) => {
+            console.log(response.data)
+            setTodolists(todolists.filter(todolist => todolist.todolistID !== todolistID))
+        }).then(() => {
+            axios.get<Todolist[]>("https://social-network.samuraijs.com/api/1.1/todo-lists", {
+                headers: {
+                    Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc"
+                }
+            })
+                .then((response) => {
+                    setTodolists(response.data)
+                })
+        })
     }
 
     const updateTodolistHandler = (id: string, title: string) => {
@@ -117,8 +136,20 @@ export type FieldError = {
     error: string
     field: string
 }
-export type CreateTodolistResponse = {
+export type CreateDeleteTodolistResponse = {
     data: { item: Todolist }
+    fieldsErrors: FieldError[]
+    messages: string[]
+    resultCode: number
+}
+export type DeleteTodolistResponse = {
+    data: {}
+    fieldsErrors: string[]
+    messages: string[]
+    resultCode: number
+}
+export type UpdateTodolistResponse = {
+    data: {}
     fieldsErrors: FieldError[]
     messages: string[]
     resultCode: number
