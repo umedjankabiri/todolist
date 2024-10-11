f23b318ebd8df135e7d51c8118e976baa869bc1d
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-12 01:07:34 +0300

    Refactor import statements in App.tsx
    Consolidate imports for Header and Main components to improve code readability and maintainability. This change simplifies the import declaration by using a single line import from "common/components".

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,9 @@
 import 'App/App.css'
 import {CssBaseline, ThemeProvider} from "@mui/material";
-import {Header} from "common/components/Header/Header.tsx";
-import {Main} from "common/components/Main/Main.tsx";
 import {toggleTheme} from "common/utils/toggleTheme.ts";
 import {useAppSelector} from "common/hooks/useAppSelector.ts";
 import {selectThemeMode} from "common/selectors/themesSelectors.ts";
+import {Header, Main} from "common/components";
 
 // Business Logic Layer (BLL)
 export const App = () => {
