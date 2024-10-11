62d16a3f2210ca8494a49e38b134bfbaafcc997b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 22:05:38 +0300

    Add types for Todolist API props
    Created `FieldError` and `BaseResponse` types to define the structure for error handling and response data from the Todolist API. These types will ensure consistency and better type checking in API interactions.

---
+++ src/common/types/Todolists/todolistsApiProps.ts
@@ -0,0 +1,10 @@
+export type FieldError = {
+    error: string
+    field: string
+}
+export type BaseResponse<D = {}> = {
+    resultCode: number
+    messages: string[]
+    fieldsErrors: FieldError[]
+    data: D
+}
