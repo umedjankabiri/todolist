388429ce94c6dcd45f66e17b65098c5fd8508fd6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 21:38:33 +0300

    Refactor task status update to use tasksApi
    Replaced direct axios request with tasksApi.changeTaskStatus for updating the task status. This change reduces the code complexity and improves the maintainability of the HTTP request logic.

--- src/App/AppHttpRequest.tsx
+++ src/App/AppHttpRequest.tsx
@@ -84,13 +84,7 @@ export const AppHttpRequests = () => {
             priority: task.priority,
             startDate: task.startDate,
         }
-        axios.put<UpdateTaskResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${task.todoListId}/tasks/${task.id}`,
-            model, {
-                headers: {
-                    Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
-                    "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
-                }
-            }).then(() => {
+        tasksApi.changeTaskStatus(task, model).then(() => {
             setTasks({
                 ...tasks, [task.todoListId]: tasks[task.todoListId]
                     .map(t => t.id === task.id ? {...t, ...model} : t)
