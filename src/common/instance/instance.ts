0eeaccae5ecfde5aa1d6f3a1e2d3efec3bc100c5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 20:58:11 +0300

    Add Axios instance configuration file
    Create a new Axios instance with predefined baseURL and headers for authorization and API key. This will standardize our API requests and facilitate easier maintenance of request configurations.

---
+++ src/common/instance/instance.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: "https://social-network.samuraijs.com/api/1.1/",
+    headers: {
+        Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
+        "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
+    }
+})
