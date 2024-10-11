68823b113a771b2873787033f39c5620a5c5b964
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 21:21:18 +0300

    Rename parameter in getTasks method
    Renamed parameter from `id` to `todolistId` for better clarity. This change helps in understanding that the parameter specifically represents the ID of a todolist.

--- src/features/ui/Todolists/api/tasksApi.ts
+++ src/features/ui/Todolists/api/tasksApi.ts
@@ -2,7 +2,7 @@ import {GetTasksResponse} from "features/ui/Todolists/types/tasksApi.types.ts";
 import {tasksInstance} from "common/instance/tasksInstance.ts";
 
 export const tasksApi = {
-    getTasks(id: string) {
-        return tasksInstance(id).get<GetTasksResponse>(`tasks`)
+    getTasks(todolistId: string) {
+        return tasksInstance(todolistId).get<GetTasksResponse>(`tasks`)
     }
 }
