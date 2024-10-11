790c64b0289ab6a51c861d10a679d6a684eee114
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 20:51:14 +0300

    Add create and delete todolist API methods
    Implemented methods for creating and deleting todolists in the `todolistsApi` module. Updated imports to include necessary response types.

--- src/features/ui/Todolists/api/todolistsApi.ts
+++ src/features/ui/Todolists/api/todolistsApi.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
-import {Todolist, UpdateTodolistResponse} from "features/ui/Todolists/types/todolistApi.types.ts";
+import {
+    CreateDeleteTodolistResponse,
+    DeleteTodolistResponse,
+    Todolist,
+    UpdateTodolistResponse
+} from "features/ui/Todolists/types/todolistApi.types.ts";
 
 export const todolistsApi = {
     getTodolists() {
@@ -11,6 +16,15 @@ export const todolistsApi = {
                 }
             });
     },
+    createTodolist(title: string) {
+        return axios.post<CreateDeleteTodolistResponse>("https://social-network.samuraijs.com/api/1.1/todo-lists",
+            {title: title}, {
+                headers: {
+                    Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
+                    "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
+                }
+            })
+    },
     updateTodolist(payload: {id: string, title: string}) {
         const {id, title} = payload;
         return axios.put<UpdateTodolistResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, {title: title}, {
@@ -20,5 +34,12 @@ export const todolistsApi = {
             }
         })
     },
-
+    deleteTodolist(todolistID: string) {
+        return axios.delete<DeleteTodolistResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, {
+            headers: {
+                Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
+                "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
+            }
+        })
+    }
 }
