85a9c63a0178f327ccdb4b24ab32acce8897b8cc
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 23:20:57 +0300

    Refactor to use environment variables for config
    Updated the axios instance configuration to retrieve base URL, token, and API key from environment variables instead of hardcoding them. This enhances security and flexibility for different deployment environments.

--- src/common/instance/todolistInstance.ts
+++ src/common/instance/todolistInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const todolistInstance = axios.create({
-    baseURL: "https://social-network.samuraijs.com/api/1.1/",
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
-        Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
-        "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+        "api-key": process.env.REACT_APP_API_KEY,
     }
 })
