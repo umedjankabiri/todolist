fb036781fb4f1541a8d7557aa150fc8123c20f3a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 21:04:07 +0300

    Refactor todolist update to use axios instance
    Replaced direct axios call with predefined instance for updating todolists. This improves code readability and simplifies maintenance by centralizing Axios configuration.

--- src/features/ui/Todolists/api/todolistsApi.ts
+++ src/features/ui/Todolists/api/todolistsApi.ts
@@ -16,12 +16,7 @@ export const todolistsApi = {
     },
     updateTodolist(payload: {id: string, title: string}) {
         const {id, title} = payload;
-        return axios.put<UpdateTodolistResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`, {title: title}, {
-            headers: {
-                Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
-                "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
-            }
-        })
+        return instance.put<UpdateTodolistResponse>(`todo-lists/${id}`, {title: title},)
     },
     deleteTodolist(todolistID: string) {
         return axios.delete<DeleteTodolistResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistID}`, {
