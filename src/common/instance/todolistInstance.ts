fd2a2362c6ed659ff70eca883c08e6baaeb092e7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 23:56:55 +0300

    Update environment variable names in axios instance
    Changed VITE_API_URL to VITE_BASE_URL and VITE_API_TOKEN to VITE_AUTH_TOKEN in todolistInstance.ts. This ensures compatibility with the new environment variable naming convention.

--- src/common/instance/todolistInstance.ts
+++ src/common/instance/todolistInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const todolistInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+        Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
         "api-key": import.meta.env.VITE_API_KEY
     }
 })
