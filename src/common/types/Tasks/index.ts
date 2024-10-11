954b75c39ad8cff1b558b307b5013c645aae5a1a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-12 00:54:29 +0300

    Add missing type exports for Tasks
    Previously, only TasksResponse was exported from TasksApiProps. This update ensures that GetTasksResponse, TasksKeyState, DomainTask, and UpdateTaskModel are also available for import in other modules.

--- src/common/types/Tasks/index.ts
+++ src/common/types/Tasks/index.ts
@@ -1,1 +1,5 @@
+export type {GetTasksResponse} from "./TasksApiProps";
+export type {TasksKeyState} from "./TasksApiProps";
+export type {DomainTask} from "./TasksApiProps";
+export type {UpdateTaskModel} from "./TasksApiProps";
 export type {TasksResponse} from "./TasksApiProps";
