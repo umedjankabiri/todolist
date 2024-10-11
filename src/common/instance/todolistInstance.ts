5e89e090609484b78b4adc8a691affefbaf34a47
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 21:08:00 +0300

    Rename instance to todolistInstance
    Updated the axios instance name for clarity in the context of a to-do list application. This change improves code readability and intent without altering functionality.

--- src/common/instance/todolistInstance.ts
+++ src/common/instance/todolistInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const instance = axios.create({
+export const todolistInstance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.1/",
     headers: {
         Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
