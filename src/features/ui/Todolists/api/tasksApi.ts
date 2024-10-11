9176b82e7556976addcb31462204208ede82c6ed
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 22:54:46 +0300

    Refactor task API response types
    Updated imports and response types to use unified `TasksResponse` type. This ensures consistency across task-related API calls, improving maintainability and reducing potential type conflicts.

--- src/features/ui/Todolists/api/tasksApi.ts
+++ src/features/ui/Todolists/api/tasksApi.ts
@@ -1,25 +1,20 @@
-import {
-    CreateTasksResponse, DeleteTaskResponse,
-    DomainTask,
-    GetTasksResponse, UpdateTaskModel,
-    UpdateTaskResponse
-} from "features/ui/Todolists/types/tasksApi.types.ts";
 import {tasksInstance} from "common/instance/tasksInstance.ts";
+import {DomainTask, GetTasksResponse, TasksResponse, UpdateTaskModel} from "common/types/Tasks/TasksApiProps.ts";
 
 export const tasksApi = {
     getTasks(todolistId: string) {
         return tasksInstance(todolistId).get<GetTasksResponse>(`tasks`)
     },
     createTask(title: string, todolistId: string) {
-        return tasksInstance(todolistId).post<CreateTasksResponse>(`tasks`, {title: title})
+        return tasksInstance(todolistId).post<TasksResponse<{item: DomainTask}>>(`tasks`, {title: title})
     },
     changeTaskStatus(task: DomainTask, model: UpdateTaskModel) {
-        return tasksInstance(task.todoListId).put<UpdateTaskResponse>(`tasks/${task.id}`, model)
+        return tasksInstance(task.todoListId).put<TasksResponse<{item: DomainTask}>>(`tasks/${task.id}`, model)
     },
     changeTaskTitle(title: string, task: DomainTask) {
-        return tasksInstance(task.todoListId).put<UpdateTaskResponse>(`tasks/${task.id}`, {title: title})
+        return tasksInstance(task.todoListId).put<TasksResponse>(`tasks/${task.id}`, {title: title})
     },
     deleteTask(taskId: string, todolistId: string) {
-        return tasksInstance(todolistId).delete<DeleteTaskResponse>(`tasks/${taskId}`)
+        return tasksInstance(todolistId).delete<TasksResponse>(`tasks/${taskId}`)
     }
 }
