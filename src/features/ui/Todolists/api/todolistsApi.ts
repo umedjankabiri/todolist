70d94a5b9eb1415d2249a85559e58837f560410e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 22:24:14 +0300

    Update API response types in Todolists API
    Changed the response type from BaseResponse to TodolistsResponse in all API calls within the todolistsApi object. This ensures consistency and correctness in handling API responses.

--- src/features/ui/Todolists/api/todolistsApi.ts
+++ src/features/ui/Todolists/api/todolistsApi.ts
@@ -1,18 +1,18 @@
 import {todolistInstance} from "common/instance/todolistInstance.ts";
-import {BaseResponse, Todolist} from "common/types/Todolists/TodolistsApiProps.ts";
+import {TodolistsResponse, Todolist} from "common/types/Todolists/TodolistsApiProps.ts";
 
 export const todolistsApi = {
     getTodolists() {
         return todolistInstance.get<Todolist[]>("todo-lists");
     },
     createTodolist(title: string) {
-        return todolistInstance.post<BaseResponse<{ item: Todolist }>>(`todo-lists`, {title})
+        return todolistInstance.post<TodolistsResponse<{ item: Todolist }>>(`todo-lists`, {title})
     },
     updateTodolist(payload: { id: string, title: string }) {
         const {id, title} = payload;
-        return todolistInstance.put<BaseResponse>(`todo-lists/${id}`, {title: title},)
+        return todolistInstance.put<TodolistsResponse>(`todo-lists/${id}`, {title: title},)
     },
     deleteTodolist(todolistID: string) {
-        return todolistInstance.delete<BaseResponse>(`todo-lists/${todolistID}`)
+        return todolistInstance.delete<TodolistsResponse>(`todo-lists/${todolistID}`)
     }
 }
