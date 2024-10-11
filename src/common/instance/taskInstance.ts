f9c52d2d03a2841400983428905794a242069286
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 23:34:12 +0300

    Update auth token environment variable name
    Changed the environment variable for the authorization token from `REACT_APP_TOKEN` to `REACT_APP_AUTH_TOKEN` in the `taskInstance.ts` file. This adjustment ensures consistency with the naming convention used across the project.

--- src/common/instance/taskInstance.ts
+++ src/common/instance/taskInstance.ts
@@ -4,7 +4,7 @@ export const taskInstance = (todolistId: string) => {
     return axios.create({
         baseURL: `${process.env.REACT_APP_BASE_URL}/${todolistId}/`,
         headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+            Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
             "api-key": process.env.REACT_APP_API_KEY,
         }
     })
