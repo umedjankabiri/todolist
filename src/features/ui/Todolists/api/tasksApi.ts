7bd6fe4631ed01bb168b02acb90d0682a730c95b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 21:17:20 +0300

    Refactor tasks API to use tasksInstance
    Replaced direct axios calls with tasksInstance for getTasks in tasksApi. This change improves code maintainability and centralizes instance configuration.

--- src/features/ui/Todolists/api/tasksApi.ts
+++ src/features/ui/Todolists/api/tasksApi.ts
@@ -1,13 +1,8 @@
-import axios from "axios";
 import {GetTasksResponse} from "features/ui/Todolists/types/tasksApi.types.ts";
+import {tasksInstance} from "common/instance/tasksInstance.ts";
 
 export const tasksApi = {
     getTasks(id: string) {
-        return axios.get<GetTasksResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}/tasks`, {
-            headers: {
-                Authorization: "Bearer ce08439a-a32a-4af0-8da4-627c8240efbc",
-                "api-key": "60e0596e-352f-4b57-8e3f-8be82fb42652"
-            }
-        })
+        return tasksInstance(id).get<GetTasksResponse>(`tasks`)
     }
 }
