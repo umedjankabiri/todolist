cd41e96e6fd64e658b0bcf87f24f60c35eab0845
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-12 01:07:26 +0300

    Refactor import path for EditableSpan component
    Moved the EditableSpan component import to a more general path under common/components. This improves the modularity and maintainability of the code.

--- src/features/ui/Todolists/Todolist/Tasks/Task/Task.tsx
+++ src/features/ui/Todolists/Todolist/Tasks/Task/Task.tsx
@@ -6,7 +6,7 @@ import {Checkbox, ListItem} from "@mui/material";
 import {getListItemSx} from "common/components/EditableSpan/EditableSpan.styles.ts";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import {EditableSpan} from "common/components/EditableSpan";
+import {EditableSpan} from "common/components";
 
 export const Task: FC<TodolistTaskProps> = ({todolist, task}) => {
     const dispatch = useAppDispatch()
