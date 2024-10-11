6f98f9d36ab1fb587ab3a447ff833e8c7fea276a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-11 21:29:24 +0300

    Add createTask method to tasksApi
    Implement createTask method to enable creating tasks in the tasks API. This change uses the tasksInstance to post a new task with a specified title to the given todolistId.

--- src/features/ui/Todolists/api/tasksApi.ts
+++ src/features/ui/Todolists/api/tasksApi.ts
@@ -1,8 +1,11 @@
-import {GetTasksResponse} from "features/ui/Todolists/types/tasksApi.types.ts";
+import {CreateTasksResponse, GetTasksResponse} from "features/ui/Todolists/types/tasksApi.types.ts";
 import {tasksInstance} from "common/instance/tasksInstance.ts";
 
 export const tasksApi = {
     getTasks(todolistId: string) {
         return tasksInstance(todolistId).get<GetTasksResponse>(`tasks`)
+    },
+    createTask(title: string, todolistId: string) {
+        return tasksInstance(todolistId).post<CreateTasksResponse>(`tasks`, {title: title})
     }
 }
