3c3d99baf0c2dcfb4e83824b72e6209b0faf7f7f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-12 04:59:36 +0300

    Add central exports file to consolidate imports
    Created a new index.ts file under src/common to centralize exports from various hooks, instances, selectors, and utility enums. This refactoring aims to simplify and streamline import statements across the codebase.

---
+++ src/common/index.ts
@@ -0,0 +1,10 @@
+export { useAppDispatch } from "./hooks/useAppDispatch.ts";
+export { useAppSelector } from "./hooks/useAppSelector.ts";
+export { todolistInstance } from "./instance/todolistInstance.ts";
+export { taskInstance } from "./instance/taskInstance.ts";
+export { selectThemeMode } from "./selectors/themesSelectors.ts";
+export { selectTodolists } from "./selectors/TodolistsSelectors.ts";
+export { selectTasks } from "./selectors/taskSelectors.ts";
+export { toggleTheme } from "./utils/toggleTheme.ts";
+export { TaskStatus } from "./utils/enums/enumTaskStatus.ts";
+export { TaskPriority } from "./utils/enums/enumTaskPriority.ts";
