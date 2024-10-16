3c9a75f3bb4104a7d1d37978b6d9b50bc10a286b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 17:40:10 +0300

    Refactor import order and fix typo in addTaskHandler
    Reorder `TodolistProps` import for better organization and correct the `todolistID` property to `id` in the `addTaskHandler` function.

--- src/features/ui/Todolists/Todolist/Todolist.tsx
+++ src/features/ui/Todolists/Todolist/Todolist.tsx
@@ -1,4 +1,3 @@
-import { TodolistProps } from "common/types/Todolists/Todolist/TodolistProps.ts";
 import { FC } from "react";
 import { AddItemForm } from "common/components/AddItemForm/AddItemForm.tsx";
 import { FilterButtons } from "features/ui/Todolists/Todolist/FilterButtons/FilterButtons.tsx";
@@ -6,11 +5,12 @@ import { useAppDispatch } from "common/hooks/useAppDispatch.ts";
 import { addTaskAC } from "features/model/tasksReducer/tasksReducer.ts";
 import { TodolistTitle } from "features/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx";
 import { Tasks } from "features/ui/Todolists/Todolist/Tasks/Tasks.tsx";
+import { TodolistProps } from "common/types/Todolists/Todolist/TodolistProps.ts";
 
 export const Todolist: FC<TodolistProps> = ({ todolist }) => {
   const dispatch = useAppDispatch();
 
-  const addTaskHandler = (title: string) => dispatch(addTaskAC({ todolistID: todolist.todolistID, title: title }));
+  const addTaskHandler = (title: string) => dispatch(addTaskAC({ todolistID: todolist.id, title: title }));
 
   return (
     <div>
