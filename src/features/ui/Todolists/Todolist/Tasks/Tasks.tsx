06ec011ee011e47e0f8bf7d67ddfa62e579e39d8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 19:18:24 +0300

    Fetch tasks on Todolist mount
    Added a useEffect hook to dispatch fetchTasksTC when the Todolist component mounts. This ensures that tasks are fetched from the server when the Todolist is initialized.

--- src/features/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,13 +1,21 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { TodolistProps } from "common/types/Todolists/Todolist/TodolistProps.ts";
 import { List } from "@mui/material";
 import { Task } from "features/ui/Todolists/Todolist/Tasks/Task/Task.tsx";
 import { useAppSelector } from "common/hooks/useAppSelector.ts";
 import { selectTasks } from "common/selectors/taskSelectors.ts";
 import { TaskStatus } from "common";
+import { AppDispatch } from "App/store.ts";
+import { useDispatch } from "react-redux";
+import { fetchTasksTC } from "features/model/thunks/tasksThunks.ts";
 
 export const Tasks: FC<TodolistProps> = ({ todolist }) => {
   const tasks = useAppSelector(selectTasks);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchTasksTC(todolist.id));
+  }, [dispatch]);
 
   const { filter } = todolist;
 
