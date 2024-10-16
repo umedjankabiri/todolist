b46ae572e639838d5bb38f90acce6e02a360f9fd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 19:31:51 +0300

    Refactor task removal to use thunk action
    Replaced the synchronous action creator with a thunk for task removal. This aligns with the existing async handling pattern for task-related operations, improving consistency and potentially enabling more complex side effects during task deletion.

--- src/features/ui/Todolists/Todolist/Tasks/Task/Task.tsx
+++ src/features/ui/Todolists/Todolist/Tasks/Task/Task.tsx
@@ -1,20 +1,21 @@
 import { ChangeEvent, FC } from "react";
 import { useAppDispatch } from "common/hooks/useAppDispatch.ts";
 import { TodolistTaskProps } from "common/types/Todolists/TodolistTask/TodolistTaskProps.ts";
-import { changeTaskStatusAC, changeTaskTitleAC, removeTaskAC } from "features/model/tasksReducer/tasksReducer.ts";
+import { changeTaskStatusAC, changeTaskTitleAC } from "features/model/tasksReducer/tasksReducer.ts";
 import { Checkbox, ListItem } from "@mui/material";
 import { getListItemSx } from "common/components/EditableSpan/EditableSpan.styles.ts";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { EditableSpan } from "common/components";
 import { TaskStatus } from "common";
+import { deleteTaskTC } from "features/model/thunks/tasksThunks.ts";
 
 export const Task: FC<TodolistTaskProps> = ({ todolist, task }) => {
   const dispatch = useAppDispatch();
 
   const { id } = task;
 
-  const removeTaskHandler = () => dispatch(removeTaskAC({ todolistId: todolist.id, taskId: id }));
+  const removeTaskHandler = () => dispatch(deleteTaskTC({ todolistId: todolist.id, taskId: id }));
   const changeTaskStatusHandler = (event: ChangeEvent<HTMLInputElement>) =>
     dispatch(
       changeTaskStatusAC({
