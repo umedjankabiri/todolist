04dc03999da7adcbed69dd5b60c1dd2a0709acce
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 20:14:41 +0300

    Add thunk to change task status
    Introduce `changeTaskStatusTC` thunk for updating task status. This includes necessary imports and dispatches the `changeTaskStatusAC` action after updating the task via the API.

--- src/features/model/thunks/tasksThunks.ts
+++ src/features/model/thunks/tasksThunks.ts
@@ -1,6 +1,9 @@
 import { Dispatch } from "redux";
 import { tasksApi } from "features/ui/Todolists/api/tasksApi.ts";
-import { addTaskAC, removeTaskAC, setTasksAC } from "features/model/tasksReducer/tasksReducer.ts";
+import { addTaskAC, changeTaskStatusAC, removeTaskAC, setTasksAC } from "features/model/tasksReducer/tasksReducer.ts";
+import { RootState } from "App/store.ts";
+import { TaskStatus } from "common";
+import { UpdateTaskModel } from "common/types/Tasks";
 
 export const fetchTasksTC = (todolistID: string) => (dispatch: Dispatch) => {
   tasksApi.getTasks(todolistID).then((response) => {
@@ -18,3 +21,24 @@ export const addTaskTC = (args: { title: string; todolistId: string }) => (dispa
     dispatch(addTaskAC({ task: response.data.data.item }));
   });
 };
+export const changeTaskStatusTC =
+  (args: { todolistId: string; taskId: string; status: TaskStatus }) =>
+  (dispatch: Dispatch, getState: () => RootState) => {
+    const { todolistId, taskId, status } = args;
+    const stateTasks = getState().tasks;
+    const currentTask = stateTasks[todolistId];
+    const task = currentTask.find((t) => t.id === taskId);
+    if (task) {
+      const model: UpdateTaskModel = {
+        status: status,
+        title: task.title,
+        description: task.description,
+        priority: task.priority,
+        startDate: task.startDate,
+      };
+
+      tasksApi.updateTask({ todolistId, taskId, model }).then(() => {
+        dispatch(changeTaskStatusAC(args));
+      });
+    }
+  };
