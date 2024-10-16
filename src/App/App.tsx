e00d926b88c7c2fdcd5baaddd18aa010bacb6e00
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 17:57:14 +0300

    Fetch todolists on component mount
    Added useEffect to dispatch fetchTodolistsTC when the App component mounts. This ensures that the todolists are fetched immediately upon loading the application, improving initial data availability.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,11 +4,20 @@ import { toggleTheme } from "common/utils/toggleTheme.ts";
 import { useAppSelector } from "common/hooks/useAppSelector.ts";
 import { selectThemeMode } from "common/selectors/themesSelectors.ts";
 import { Header, Main } from "common/components";
+import { useEffect } from "react";
+import { fetchTodolistsTC } from "features/model/todolistsThunks/todolistsThunks.ts";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "App/store.ts";
 
 // Business Logic Layer (BLL)
 export const App = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const themeMode = useAppSelector(selectThemeMode);
   const theme = toggleTheme(themeMode);
+
+  useEffect(() => {
+    dispatch(fetchTodolistsTC);
+  }, []);
 
   // main layout
   return (
