1ef6eb5a7bdeda77014bdb2762f75ef08556f9fe
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 18:27:47 +0300

    Rename function and file paths in todolistsThunks.
    Renamed the `fetchTodolistsTC` function to `fetchTodolistsThunk` for clarity. Also updated the file path from `model/todolistsThunks/todolistsThunks.ts` to `model/thunks/todolistsThunks.ts`.

--- src/features/model/thunks/todolistsThunks.ts
+++ src/features/model/thunks/todolistsThunks.ts
@@ -2,7 +2,7 @@ import { Dispatch } from "@reduxjs/toolkit";
 import { todolistsApi } from "features/ui/Todolists/api/todolistsApi.ts";
 import { setTodolistsAC } from "features/model/todolistsReducer/todolistsReducer.ts";
 
-export const fetchTodolistsTC = (dispatch: Dispatch) => {
+export const fetchTodolistsThunk = (dispatch: Dispatch) => {
   todolistsApi.getTodolists().then((response) => {
     dispatch(setTodolistsAC(response.data));
   });
