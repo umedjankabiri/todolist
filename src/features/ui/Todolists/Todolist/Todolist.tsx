d72445da51a531efd6e1de416af7feb280d8b75e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 19:38:13 +0300

    Replace addTaskAC with addTaskTC in Todolist component
    Swapped out the action creator addTaskAC for the thunk addTaskTC in the Todolist component. This change alters the way tasks are added by dispatching a thunk instead of a simple action.

--- src/features/ui/Todolists/Todolist/Todolist.tsx
+++ src/features/ui/Todolists/Todolist/Todolist.tsx
@@ -2,15 +2,15 @@ import { FC } from "react";
 import { AddItemForm } from "common/components/AddItemForm/AddItemForm.tsx";
 import { FilterButtons } from "features/ui/Todolists/Todolist/FilterButtons/FilterButtons.tsx";
 import { useAppDispatch } from "common/hooks/useAppDispatch.ts";
-import { addTaskAC } from "features/model/tasksReducer/tasksReducer.ts";
 import { TodolistTitle } from "features/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx";
 import { Tasks } from "features/ui/Todolists/Todolist/Tasks/Tasks.tsx";
 import { TodolistProps } from "common/types/Todolists/Todolist/TodolistProps.ts";
+import { addTaskTC } from "features/model/thunks/tasksThunks.ts";
 
 export const Todolist: FC<TodolistProps> = ({ todolist }) => {
   const dispatch = useAppDispatch();
 
-  const addTaskHandler = (title: string) => dispatch(addTaskAC({ todolistId: todolist.id, title: title }));
+  const addTaskHandler = (title: string) => dispatch(addTaskTC({ todolistId: todolist.id, title: title }));
 
   return (
     <div>
