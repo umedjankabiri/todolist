01ea40fcfa2343d1d969e0b9840b51e6eeaf6712
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 17:39:44 +0300

    Rename and update TodolistsActionsProps file
    Moved TodolistsActionsProps to a new directory for better organization. Added a new action type `SetTodolistsActionProps` derived from the `setTodolistsAC` function.

--- src/common/types/Todolists/TodolistsActions/TodolistsActionsProps.ts
+++ src/common/types/Todolists/TodolistsActions/TodolistsActionsProps.ts
@@ -3,14 +3,17 @@ import {
   changeTodolistFilterAC,
   changeTodolistTitleAC,
   removeTodolistAC,
+  setTodolistsAC,
 } from "features/model/todolistsReducer/todolistsReducer.ts";
 
 export type RemoveTodolistActionProps = ReturnType<typeof removeTodolistAC>;
 export type AddTodolistActionProps = ReturnType<typeof addTodolistAC>;
 export type ChangeTodolistStatusActionProps = ReturnType<typeof changeTodolistTitleAC>;
 export type ChangeTodolistTitleActionProps = ReturnType<typeof changeTodolistFilterAC>;
+export type SetTodolistsActionProps = ReturnType<typeof setTodolistsAC>;
 export type TodolistsActionsProps =
   | RemoveTodolistActionProps
   | AddTodolistActionProps
   | ChangeTodolistStatusActionProps
-  | ChangeTodolistTitleActionProps;
+  | ChangeTodolistTitleActionProps
+  | SetTodolistsActionProps;
