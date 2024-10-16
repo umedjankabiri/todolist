07a4e277dc90e0e2eebfe585108ca32fa015b0e4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 18:13:20 +0300

    Refactor store to use redux-thunk middleware
    Updated the store configuration to import and apply `redux-thunk` middleware. This enables handling asynchronous actions within the Redux store.

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,14 +1,15 @@
-import { combineReducers, legacy_createStore, ThunkDispatch, UnknownAction } from "@reduxjs/toolkit";
+import { applyMiddleware, combineReducers, legacy_createStore, UnknownAction } from "redux";
 import { todolistsReducer } from "features/model/todolistsReducer/todolistsReducer.ts";
 import { tasksReducer } from "features/model/tasksReducer/tasksReducer.ts";
 import { themeReducer } from "features/model/themeReducer/themeReducer.ts";
+import { ThunkDispatch, thunk } from "redux-thunk";
 
 const rootReducers = combineReducers({
   todolists: todolistsReducer,
   tasks: tasksReducer,
   themes: themeReducer,
 });
-export const store = legacy_createStore(rootReducers);
+export const store = legacy_createStore(rootReducers, {}, applyMiddleware(thunk));
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>;
 
