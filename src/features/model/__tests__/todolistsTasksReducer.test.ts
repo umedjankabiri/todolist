00715f5f2aa1f39c42d230ff8038d474a82101a6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 17:38:59 +0300

    Refactor todolistsTasksReducer.test.ts to use DomainTodolist type
    Updated the test file to use the `DomainTodolist` type for better alignment with existing API types and removed an obsolete import. Adjusted the property names to match the updated action payload structure.

--- src/features/model/__tests__/todolistsTasksReducer.test.ts
+++ src/features/model/__tests__/todolistsTasksReducer.test.ts
@@ -1,11 +1,11 @@
 import { TasksStateProps } from "common/types/Tasks/TasksStateProps.ts";
-import { TodolistsProps } from "common/types/Todolists/TodolistsProps.ts";
 import { addTodolistAC, todolistsReducer } from "features/model/todolistsReducer/todolistsReducer.ts";
 import { tasksReducer } from "features/model/tasksReducer/tasksReducer.ts";
+import { DomainTodolist } from "common/types/Todolists/TodolistsApiProps.ts";
 
 test("ids should be equals", () => {
   const startTasksState: TasksStateProps = {};
-  const startTodolistsState: TodolistsProps[] = [];
+  const startTodolistsState: DomainTodolist[] = [];
 
   const action = addTodolistAC("What to read");
 
@@ -14,8 +14,8 @@ test("ids should be equals", () => {
 
   const keys = Object.keys(endTasksState);
   const idFromTasks = keys[0];
-  const idFromTodolists = endTodolistsState[0].todolistID;
+  const idFromTodolists = endTodolistsState[0].id;
 
-  expect(idFromTasks).toBe(action.payload.todolistID);
-  expect(idFromTodolists).toBe(action.payload.todolistID);
+  expect(idFromTasks).toBe(action.payload.id);
+  expect(idFromTodolists).toBe(action.payload.id);
 });
