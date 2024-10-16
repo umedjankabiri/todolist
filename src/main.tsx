d37cbbddabdb91f45254e07a8cd0787dbe5039e0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 17:41:09 +0300

    Switch to App component in main.tsx
    Replaced AppHttpRequests with App in main.tsx for rendering. This ensures the application initializes with the primary App component.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "App/store.ts";
-import { AppHttpRequests } from "App/AppHttpRequest.tsx";
+import { App } from "App/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    {/*<App/>*/}
-    <AppHttpRequests />
+    <App />
+    {/*<AppHttpRequests />*/}
   </Provider>
 );
