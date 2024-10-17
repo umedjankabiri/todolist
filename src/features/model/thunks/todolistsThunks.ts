ece8ca851f9948042f486786954d9e7b04a1b018
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-17 13:40:51 +0300

    Add thunk for creating a new todolist
    Extended the thunks with `addTodolistTC` to handle todolist creation using `todolistsApi.createTodolist`. This allows dispatching the `addTodolistAC` action to update the state with the new todolist.

--- src/features/model/thunks/todolistsThunks.ts
+++ src/features/model/thunks/todolistsThunks.ts
@@ -1,9 +1,14 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import { todolistsApi } from "features/ui/Todolists/api/todolistsApi.ts";
-import { setTodolistsAC } from "features/model/todolistsReducer/todolistsReducer.ts";
+import { addTodolistAC, setTodolistsAC } from "features/model/todolistsReducer/todolistsReducer.ts";
 
 export const fetchTodolistsTC = () => (dispatch: Dispatch) => {
   todolistsApi.getTodolists().then((response) => {
     dispatch(setTodolistsAC(response.data));
+  });
+};
+export const addTodolistTC = (title: string) => (dispatch: Dispatch) => {
+  todolistsApi.createTodolist(title).then((response) => {
+    dispatch(addTodolistAC(response.data.data.item));
   });
 };
