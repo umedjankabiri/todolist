55bafd5cc4e7387cc6935f9b121dad4b3b20271f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-17 13:41:14 +0300

    Add deleteTodolist thunk
    Implement a thunk for deleting todolists by dispatching removeTodolistAC after successful API call. This addition enhances state management related to todolist deletions.

--- src/features/model/thunks/todolistsThunks.ts
+++ src/features/model/thunks/todolistsThunks.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import { todolistsApi } from "features/ui/Todolists/api/todolistsApi.ts";
-import { addTodolistAC, setTodolistsAC } from "features/model/todolistsReducer/todolistsReducer.ts";
+import { addTodolistAC, removeTodolistAC, setTodolistsAC } from "features/model/todolistsReducer/todolistsReducer.ts";
 
 export const fetchTodolistsTC = () => (dispatch: Dispatch) => {
   todolistsApi.getTodolists().then((response) => {
@@ -10,5 +10,10 @@ export const fetchTodolistsTC = () => (dispatch: Dispatch) => {
 export const addTodolistTC = (title: string) => (dispatch: Dispatch) => {
   todolistsApi.createTodolist(title).then((response) => {
     dispatch(addTodolistAC(response.data.data.item));
+  });
+};
+export const deleteTodolistTC = (id: string) => (dispatch: Dispatch) => {
+  todolistsApi.deleteTodolist(id).then(() => {
+    dispatch(removeTodolistAC(id));
   });
 };
