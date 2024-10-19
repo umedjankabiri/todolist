7ffab162321e66568d7e5b488b0b61bc50e472b9
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 19:54:05 +0300

    Refactor ThemeModeProps types
    Removed `initialState` import and replaced `InitialState` type with `InitialStateProps` to enhance type clarity. Adjusted type definitions for better alignment with the updated reducer action.

--- src/common/types/ThemeMode/ThemeModeProps.ts
+++ src/common/types/ThemeMode/ThemeModeProps.ts
@@ -1,6 +1,8 @@
-import { changeThemeModeAC, initialState } from "features/model/reducers/themeReducer.ts";
+import { changeThemeModeAC } from "features/model/reducers/themeReducer.ts";
 
 export type ThemeMode = "dark" | "light";
-export type InitialState = typeof initialState;
+export type InitialStateProps = {
+  themeMode: ThemeMode;
+};
 export type changeThemeModeActionProps = ReturnType<typeof changeThemeModeAC>;
 export type ActionProps = changeThemeModeActionProps;
