4d1089f7384828b8d7d26db30de863ba467854c8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 18:59:17 +0300

    Add statusReducer and action creators for status handling
    This commit introduces a new statusReducer to manage application status and error states. Two action creators, setStatusAC and setErrorAC, are also added to facilitate updates to the status and error properties, respectively. This enhances the state management by providing a dedicated reducer for status-related actions.

---
+++ src/features/model/reducers/statusReducer.ts
@@ -0,0 +1,29 @@
+import { StatusProps } from "common/types/Status/StatusProps.ts";
+import { RequestStatusProps } from "common/types/Status/RequestStatusProps.ts";
+import { StatusActions } from "common/types/Status/StatusActions.ts";
+
+const initialStatusState: StatusProps = {
+  status: "idle",
+  error: null,
+};
+
+export const statusReducer = (state = initialStatusState, action: StatusActions): StatusProps => {
+  switch (action.type) {
+    case "SET-STATUS":
+      return {
+        ...state,
+        status: action.payload.status,
+      };
+    case "SET-ERROR":
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export const setStatusAC = (status: RequestStatusProps) =>
+  ({ type: "SET-STATUS", payload: { status: status } }) as const;
+export const setErrorAC = (error: null | string) => ({ type: "SET-ERROR", payload: { error: error } }) as const;
