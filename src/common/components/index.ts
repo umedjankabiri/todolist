60faa61ed2afd959d9c4aa692e87acce424fef63
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 19:53:05 +0300

    Add ErrorSnackBar to exports in components index
    This commit adds the ErrorSnackBar component to the export list in src/common/components/index.ts. It ensures that ErrorSnackBar can be imported and used in other parts of the project.

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,5 +1,6 @@
 export { AddItemForm } from "./AddItemForm/AddItemForm";
 export { Button } from "./Button/Button";
 export { EditableSpan } from "./EditableSpan/EditableSpan";
+export { ErrorSnackBar } from "./ErrorSnackBar/ErrorSnackBar";
 export { Header } from "./Header/Header";
 export { Main } from "./Main/Main";
