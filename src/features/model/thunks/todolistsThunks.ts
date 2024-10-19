565b14cb91d7e0c8dc05b84b34dbe5da95a801a4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 19:10:53 +0300

    Set status during todolist operations
    Add dispatch calls to set loading and success statuses around fetch, add, delete, and update operations for greater transparency of current status. This enhances user feedback by indicating when the app is processing a request.

--- src/features/model/thunks/todolistsThunks.ts
+++ src/features/model/thunks/todolistsThunks.ts
@@ -6,24 +6,33 @@ import {
   removeTodolistAC,
   setTodolistsAC,
 } from "features/model/reducers/todolistsReducer.ts";
+import { setStatusAC } from "features/model/reducers/statusReducer.ts";
 
 export const fetchTodolistsTC = () => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   todolistsApi.getTodolists().then((response) => {
+    dispatch(setStatusAC("success"));
     dispatch(setTodolistsAC(response.data));
   });
 };
 export const addTodolistTC = (title: string) => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   todolistsApi.createTodolist(title).then((response) => {
+    dispatch(setStatusAC("success"));
     dispatch(addTodolistAC(response.data.data.item));
   });
 };
 export const deleteTodolistTC = (id: string) => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   todolistsApi.deleteTodolist(id).then(() => {
+    dispatch(setStatusAC("success"));
     dispatch(removeTodolistAC(id));
   });
 };
 export const updateTodolistTitleTC = (args: { id: string; title: string }) => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   todolistsApi.updateTodolist(args).then(() => {
+    dispatch(setStatusAC("success"));
     dispatch(changeTodolistTitleAC({ todolistID: args.id, title: args.title }));
   });
 };
