4a5d0e4ce5a2b7e7610440b70c52e06942ab4e06
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 19:54:24 +0300

    Add ErrorSnackBar component to App
    Integrated ErrorSnackBar to display error messages in the App component. This enhances user feedback for error states detected via the selectStatus selector.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,14 +3,16 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { toggleTheme } from "common/utils/toggleTheme.ts";
 import { useAppSelector } from "common/hooks/useAppSelector.ts";
 import { selectThemeMode } from "common/selectors/themesSelectors.ts";
-import { Header, Main } from "common/components";
+import { ErrorSnackBar, Header, Main } from "common/components";
 import { useEffect } from "react";
 import { fetchTodolistsTC } from "features/model/thunks/todolistsThunks.ts";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "App/store.ts";
+import { selectStatus } from "common";
 
 // Business Logic Layer (BLL)
 export const App = () => {
+  const { error } = useAppSelector(selectStatus);
   const dispatch = useDispatch<AppDispatch>();
   const themeMode = useAppSelector(selectThemeMode);
   const theme = toggleTheme(themeMode);
@@ -25,6 +27,7 @@ export const App = () => {
       <CssBaseline />
       <Header />
       <Main />
+      {error && <ErrorSnackBar />}
     </ThemeProvider>
   );
 };
