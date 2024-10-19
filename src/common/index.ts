d14ddc30fad8c46deefdd20204039c1510dffc32
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 19:52:56 +0300

    Add selectStatus export to index.ts
    This new export function allows modules to import selectStatus from a single entry point, improving consistency and convenience in the codebase. It aligns with existing practices for selector exports in the project.

--- src/common/index.ts
+++ src/common/index.ts
@@ -5,6 +5,7 @@ export { taskInstance } from "./instance/taskInstance.ts";
 export { selectThemeMode } from "./selectors/themesSelectors.ts";
 export { selectTodolists } from "./selectors/TodolistsSelectors.ts";
 export { selectTasks } from "./selectors/taskSelectors.ts";
+export { selectStatus } from "./selectors/statusSelector.ts";
 export { toggleTheme } from "./utils/toggleTheme.ts";
 export { TaskStatus } from "./utils/enums/enumTaskStatus.ts";
 export { TaskPriority } from "./utils/enums/enumTaskPriority.ts";
