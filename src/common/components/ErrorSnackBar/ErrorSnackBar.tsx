bacfcad86352b9cd8798d043a90b5358ec8d4baa
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-20 01:13:33 +0300

    Refactor ErrorSnackBar by removing dispatch usage
    Removed the useAppDispatch and setErrorAC to simplify the ErrorSnackBar component's logic. This change reduces unnecessary state management within the component.

--- src/common/components/ErrorSnackBar/ErrorSnackBar.tsx
+++ src/common/components/ErrorSnackBar/ErrorSnackBar.tsx
@@ -2,18 +2,14 @@ import { Alert, Snackbar } from "@mui/material";
 import { SyntheticEvent, useState } from "react";
 import { useAppSelector } from "common/hooks/useAppSelector.ts";
 import { selectStatus } from "common/selectors/statusSelector.ts";
-import { useAppDispatch } from "common/hooks/useAppDispatch.ts";
-import { setErrorAC } from "features/model/reducers/statusReducer.ts";
 
 export const ErrorSnackBar = () => {
   const { error } = useAppSelector(selectStatus);
-  const dispatch = useAppDispatch();
-  const [open, setOpen] = useState(true);
+  const [_open, setOpen] = useState(true);
 
   const onClickCloseHandler = (_event: SyntheticEvent | Event, reason?: string) => {
     if (reason === "clickaway") return;
     setOpen(false);
-    dispatch(setErrorAC("Test error message"));
   };
 
   return (
