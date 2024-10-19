b171354bd8bcf4bc981cf88dbbb5d583d04eadfd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-19 19:11:06 +0300

    Add status updates to task thunks
    Include loading and success status dispatches in task thunks (fetch, delete, add, update) to improve state management. This ensures better feedback for the user on ongoing operations.

--- src/features/model/thunks/tasksThunks.ts
+++ src/features/model/thunks/tasksThunks.ts
@@ -3,25 +3,33 @@ import { tasksApi } from "features/ui/Todolists/api/tasksApi.ts";
 import { addTaskAC, removeTaskAC, setTasksAC, updateTaskAC } from "features/model/reducers/tasksReducer.ts";
 import { RootState } from "App/store.ts";
 import { UpdateTaskDomainModel, UpdateTaskModel } from "common/types/Tasks";
+import { setStatusAC } from "features/model/reducers/statusReducer.ts";
 
 export const fetchTasksTC = (todolistID: string) => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   tasksApi.getTasks(todolistID).then((response) => {
+    dispatch(setStatusAC("success"));
     dispatch(setTasksAC({ todolistId: todolistID, tasks: response.data.items }));
   });
 };
 export const deleteTaskTC = (args: { taskId: string; todolistId: string }) => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   tasksApi.deleteTask(args).then(() => {
+    dispatch(setStatusAC("success"));
     dispatch(removeTaskAC(args));
   });
 };
 export const addTaskTC = (args: { title: string; todolistId: string }) => (dispatch: Dispatch) => {
+  dispatch(setStatusAC("loading"));
   tasksApi.createTask(args).then((response) => {
+    dispatch(setStatusAC("success"));
     dispatch(addTaskAC({ task: response.data.data.item }));
   });
 };
 export const updateTaskTC =
   (args: { todolistId: string; taskId: string; domainModel: UpdateTaskDomainModel }) =>
   (dispatch: Dispatch, getState: () => RootState) => {
+    dispatch(setStatusAC("loading"));
     const { todolistId, taskId, domainModel } = args;
     const stateTasks = getState().tasks;
     const currentTask = stateTasks[todolistId];
@@ -37,6 +45,7 @@ export const updateTaskTC =
       };
 
       tasksApi.updateTask({ todolistId, taskId, model }).then(() => {
+        dispatch(setStatusAC("success"));
         dispatch(updateTaskAC({ todolistId, taskId, domainModel }));
       });
     }
