0aebe2cc822e8945d69bb077fe8f6e046d7f7750
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-20 06:46:52 +0300

    Refactor error handling in handleServerError.ts
    Replace setStatusAC with setTodolistStatusAC to ensure specific todolist status updates on server error. This change ensures the error handling is aligned with the updated action creators and maintains consistency within the status reducer.

--- src/common/utils/handleServerError.ts
+++ src/common/utils/handleServerError.ts
@@ -1,10 +1,10 @@
 import { Dispatch } from "@reduxjs/toolkit";
-import { setErrorAC, setStatusAC } from "features/model/reducers/statusReducer.ts";
+import { setErrorAC, setTodolistStatusAC } from "features/model/reducers/statusReducer.ts";
 import { TodolistsResponse } from "common/types/Todolists/TodolistsResponseProps.ts";
 
 export const handleServerError = <T>(data: TodolistsResponse<T>, dispatch: Dispatch) => {
   if (data.messages.length) {
     dispatch(setErrorAC(data.messages[0]));
   } else dispatch(setErrorAC("Some error occurred"));
-  dispatch(setStatusAC("failed"));
+  dispatch(setTodolistStatusAC("failed"));
 };
