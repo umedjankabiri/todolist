32b8028c8be3814600ba13a06980d4a30a6b4e6c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-20 06:47:05 +0300

    Refactor error handling in handleServerNetworkError
    Updated the error status action from setStatusAC to setTodolistStatusAC for clearer action specificity. This helps maintain a more organized action structure within the statusReducer.

--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,7 +1,7 @@
 import { Dispatch } from "redux";
-import { setErrorAC, setStatusAC } from "features/model/reducers/statusReducer.ts";
+import { setErrorAC, setTodolistStatusAC } from "features/model/reducers/statusReducer.ts";
 
 export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch) => {
   dispatch(setErrorAC(error.message));
-  dispatch(setStatusAC("failed"));
+  dispatch(setTodolistStatusAC("failed"));
 };
