ab8db8f96f7fc229f8fcafd68a19a075661c678c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-20 06:44:51 +0300

    Rename setStatusAC to setTodolistStatusAC in StatusActions
    This change updates the import and type definitions in StatusActions to use setTodolistStatusAC instead of setStatusAC. This clarifies the action's purpose, ensuring better readability and maintainability of the code.

--- src/common/types/Status/StatusActions.ts
+++ src/common/types/Status/StatusActions.ts
@@ -1,6 +1,6 @@
-import { setErrorAC, setStatusAC } from "features/model/reducers/statusReducer.ts";
+import { setErrorAC, setTodolistStatusAC } from "features/model/reducers/statusReducer.ts";
 
-type SetStatusAC = ReturnType<typeof setStatusAC>;
+type SetStatusAC = ReturnType<typeof setTodolistStatusAC>;
 type SetErrorAC = ReturnType<typeof setErrorAC>;
 
 export type StatusActions = SetStatusAC | SetErrorAC;
