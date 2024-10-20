de4d936ae679e5fda1a6a14662b5db1331cd536a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-20 04:38:17 +0300

    Enhance error handling in todolist thunks
    Updated `deleteTodolistTC` and `updateTodolistTitleTC` to handle errors more robustly. Added specific checks for server response codes and network errors to ensure appropriate dispatch actions are taken.

--- src/features/model/thunks/todolistsThunks.ts
+++ src/features/model/thunks/todolistsThunks.ts
@@ -38,15 +38,34 @@ export const addTodolistTC = (title: string) => (dispatch: Dispatch) => {
 export const deleteTodolistTC = (id: string) => (dispatch: Dispatch) => {
   dispatch(setStatusAC("loading"));
   dispatch(changeTodolistEntityStatusAC({ id: id, entityStatus: "loading" }));
-  todolistsApi.deleteTodolist(id).then(() => {
-    dispatch(setStatusAC("success"));
-    dispatch(removeTodolistAC(id));
-  });
+  todolistsApi
+    .deleteTodolist(id)
+    .then((response) => {
+      if (response.data.resultCode === ResultCode.SUCCESS) {
+        dispatch(removeTodolistAC(id));
+        dispatch(setStatusAC("success"));
+      } else if (response.data.resultCode === ResultCode.ERROR) {
+        dispatch(setErrorAC(response.data.messages[0]));
+      } else handleServerError(response.data, dispatch);
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+      dispatch(changeTodolistEntityStatusAC({ id: id, entityStatus: "idle" }));
+    });
 };
 export const updateTodolistTitleTC = (args: { id: string; title: string }) => (dispatch: Dispatch) => {
   dispatch(setStatusAC("loading"));
-  todolistsApi.updateTodolist(args).then(() => {
-    dispatch(setStatusAC("success"));
-    dispatch(changeTodolistTitleAC({ todolistID: args.id, title: args.title }));
-  });
+  todolistsApi
+    .updateTodolist(args)
+    .then((response) => {
+      if (response.data.resultCode === ResultCode.SUCCESS) {
+        dispatch(changeTodolistTitleAC({ todolistID: args.id, title: args.title }));
+        dispatch(setStatusAC("success"));
+      } else if (response.data.resultCode === ResultCode.ERROR) {
+        dispatch(setErrorAC(response.data.messages[0]));
+      } else handleServerError(response.data, dispatch);
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+    });
 };
