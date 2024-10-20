80ad0a458959176475fd28e9bea9256eaccb2f7b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-20 06:56:37 +0300

    Enhance index.ts with exports from tasks and todolists reducers
    Added necessary exports from tasksThunks and todolistsReducer to index.ts. These exports include thunks for tasks and actions plus the reducer for todolists, improving module organization and accessibility.

--- src/features/model/index.ts
+++ src/features/model/index.ts
@@ -1,1 +1,11 @@
 export { fetchTodolistsTC, addTodolistTC, updateTodolistTitleTC, deleteTodolistTC } from "./thunks/todolistsThunks.ts";
+export { fetchTasksTC, addTaskTC, updateTaskTC, deleteTaskTC } from "./thunks/tasksThunks.ts";
+export {
+  setTodolistsAC,
+  addTodolistAC,
+  changeTodolistTitleAC,
+  changeTodolistFilterAC,
+  changeTodolistEntityStatusAC,
+  removeTodolistAC,
+  todolistsReducer,
+} from "./reducers/todolistsReducer.ts";
