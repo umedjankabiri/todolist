333f04afdabb3517d90263b376770e8589e19099
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-24 21:59:34 +0300

    Add Login component for user authentication
    Created a new Login component with a form for user authentication. It includes fields for email and password, a "Remember me" checkbox, and a submit button. This component uses Material-UI for styling and integrates theme toggling based on the selected theme mode.

---
+++ src/features/ui/Todolists/auth/Login/Login.tsx
@@ -0,0 +1,51 @@
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import { selectThemeMode, toggleTheme, useAppSelector } from "common";
+
+export const Login = () => {
+  const themeMode = useAppSelector(selectThemeMode);
+  const theme = toggleTheme(themeMode);
+
+  return (
+    <Grid container justifyContent={"center"}>
+      <Grid item justifyContent={"center"}>
+        <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+                href={"https://social-network.samuraijs.com/"}
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
+          <FormGroup>
+            <TextField label="Email" margin="normal" />
+            <TextField type="password" label="Password" margin="normal" />
+            <FormControlLabel label={"Remember me"} control={<Checkbox />} />
+            <Button type={"submit"} variant={"contained"} color={"primary"}>
+              Login
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
+};
