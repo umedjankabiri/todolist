e709e7833f4a5dada776ad86a4407b405abe604a
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-24 22:30:07 +0300

    Replace App with RouterProvider in main.tsx
    Switches from using the App component to RouterProvider for routing. This change incorporates the router object for managing routes, enhancing navigation and route handling across the application.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "App/store.ts";
-import { App } from "App/App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "common";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
-    {/*<AppHttpRequests />*/}
+    <RouterProvider router={router} />
   </Provider>
 );
