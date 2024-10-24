20524f8a5ba277a85e755fd918d7460a0e07e09c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-24 22:48:24 +0300

    Add 404 error page to Router
    Integrated a 404 error page component to handle unmatched routes. This provides a user-friendly message when users navigate to non-existent pages within the application. Additionally, standardized the root path definition for the Main component.

--- src/common/router/Router.tsx
+++ src/common/router/Router.tsx
@@ -3,14 +3,16 @@ import { App } from "App";
 import { Main } from "common/components";
 import { Path } from "common/utils/RouterPath.ts";
 import { Login } from "features/ui";
+import { Error404 } from "common/components/Error404/Error404.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error404 />,
     children: [
       {
-        path: Path.Main,
+        path: "/",
         element: <Main />,
       },
       {
